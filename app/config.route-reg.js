d8a83940d521ab8a699e1c2de098e5e5fbd6e39c
Bill Burke <billstron@gmail.com>
2014-05-16 16:48:45 -0400

    Working views for registration

---
+++ app/config.route-reg.js
@@ -0,0 +1,70 @@
+﻿(function () {
+    'use strict';
+
+    var app = angular.module('app');
+
+    // Collect the routes
+    app.constant('routes', getRoutes());
+
+    // Configure the routes and route resolvers
+    app.config(['$routeProvider', 'routes', routeConfigurator]);
+    function routeConfigurator($routeProvider, routes) {
+        routes.forEach(function (r) {
+            $routeProvider.when(r.url, r.config);
+        });
+        $routeProvider.otherwise({ redirectTo: '/kitchensink' });
+    }
+
+    // Define the routes
+    function getRoutes() {
+        return [
+            {
+                url: '/create-team',
+                config: {
+                    title: 'Create Team',
+                    templateUrl: 'app/registration/createteam.template.html',
+                    settings: {
+                        nav: 1,
+                        content: 'Enterprise'
+                    }
+                }
+            }, {
+                url: '/eventure',
+                config: {
+                    title: 'Eventure',
+                    templateUrl: 'app/registration/eventure.template.html',
+                    settings: {
+                        nav: 2,
+                        content: 'Events'
+                    }
+                }
+            }, {
+                url: '/eventure-list',
+                config: {
+                    title: 'Eventure List',
+                    templateUrl: 'app/registration/eventurelist.template.html',
+                    settings: {
+                        nav: 3,
+                        content: 'Participants'
+                    }
+                }
+            }, {
+                url: '/team-payment',
+                config: {
+                    title: 'Team Payment',
+                    templateUrl: 'app/registration/teampayment.template.html',
+                    settings: {
+                        nav: 4,
+                        content: 'Coupons & Addons'
+                    }
+                }
+            },{
+                url: '/kitchensink',
+                config: {
+                    title: 'Kitchen Sink',
+                    templateUrl: 'app/registration/kitchensink.html'
+                }
+            }
+        ];
+    }
+})();
