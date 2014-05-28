ab1039698ba0c4ffbfbc333b3c422c05f843bee7
boone <boone.mike@gmail.com>
2014-05-28 15:55:07 -0400

    quick check

--- app/dashboard/reporting.js
+++ app/dashboard/reporting.js
@@ -31,8 +31,5 @@
                   return vm.reports;
               });
       }
-
-
-
     }
 })();
--- app/dashboard/setup/setresource.js
+++ app/dashboard/setup/setresource.js
@@ -1,9 +1,9 @@
 ﻿(function () {
     'use strict';
-
+    
     var controllerId = 'setresource';
     angular.module('app').controller(controllerId, ['common', 'datacontext', setresource]);
-
+    
     function setresource(common, datacontext) {
         //logger.log('made it ehre!');
         var getLogFn = common.logger.getLogFn;
@@ -18,11 +18,15 @@
             common.activateController(getResource(), controllerId)
                 .then(function () { log('Activated set resource'); });
         }
-
+        
         function getResource() {
             return datacontext.getResourceById(1)
                 .then(function (data) {
-                    vm.resource = data;
+                    log('made it here!');
+                    //log('made it here!' + data.results[0].email);
+                    vm.resource = data.results[0];
+                    //vm.resource = data;
+                    //applyFilter();
                     return vm.resource;
                 });
         }
