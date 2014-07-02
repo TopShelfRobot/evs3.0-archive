489eb626f5a4223d3247f00da2c3a3befaa05191
Bill Burke <billstron@gmail.com>
2014-07-02 08:33:45 -0400

    Added name to the registraiton object

--- app/registration/models/cartModel.js
+++ app/registration/models/cartModel.js
@@ -68,7 +68,7 @@
         var currentQuantity = null;
 		
 		var setCurrentParticipant = function(part){
-			vm.currentName = config.regName; // TODO: check on this
+			vm.currentName = part.firstName + " " + part.lastName; // TODO: check on this
 			vm.currentPartId = part.id;
 			vm.currentParticipant = part;
 		};
