9a5778209af7cb59bc3674a6f1c3681d8de9b8d4
Bill Burke <billstron@gmail.com>
2014-07-02 07:04:18 -0400

    Added stripe and working through stripe service

--- app/registration/controllers/confirmController.js
+++ app/registration/controllers/confirmController.js
@@ -11,12 +11,14 @@
 		$scope.cart = cart;
 		$scope.allowPartialPayment = false;
 		$scope.allowZeroPayment = false;
+		$scope.submitDisabled = false;
         // cart.cartIsVisible = false;
 		
 		// initialize it
         datacontext.getOwnerById(config.owner.ownerId)
             .then(function (owner) {
 				$scope.owner = owner;
+				Stripe.setPublishableKey(owner.stripePublishableKey);
 	            $('#terms').popover({ title: "Terms and Conditions", html: true, content: function () { msg = '<div id="popover_content_wrapper"><p>' + owner.termsText + '</p></div>'; return $(msg).html(); }, placement: 'auto', container: 'body', trigger: 'click' });
 	            $('#refund').popover({ title: "Refund Policy", html: true, content: function () { msg = '<div id="popover_content_wrapper"><p>' + owner.refundsText + '</p></div>'; return $(msg).html(); }, placement: 'auto', container: 'body', trigger: 'click' });
 				
@@ -25,19 +27,7 @@
 				cart.processCartRules();
             });
 
-		
-        $scope.submit = function(){
-            var $form = $('#payment-form');
-
-            // Disable the submit button to prevent repeated clicks
-            $form.find('button').prop('disabled', true);
-
-            Stripe.createToken($form)
-				.then(stripeResponseHandler);
-        }
-
         var applyCoupon = function () {
-            //logger.log('validating', null, 'confirm', true);
 
             var apiUrl = config.apiPath + "/api/Coupon/Post";    //mjb
             var source = {
@@ -46,7 +36,7 @@
             };
 
             $http({type: "POST", url: apiUrl, data: source})
-				.then(function (result) {
+				.success(function (result) {
                     if (result.Amount != 0) {
                         cart.removeCoupons();
                         cart.addSurcharge('Coupon: ' + couponCode, result.Amount, 'coupon', cart.currentEventureListId(), cart.currentPartId, result.CouponId);
@@ -57,80 +47,71 @@
                         }
                     }
                 })
-				.error(function (xhr, textStatus, errorThrown) {
+				.error(function (data, status, headers, config) {
                     $scope.couponErrors = "Coupon Not Found(E1)";
                 });
         };
 
-        var stripeResponseHandler = function (status, response) {
+        var stripeSuccessHandler = function (data) {
 
-            $("#overlay").removeClass("hidden");
+			// token contains id, last4, and card type
+            var token = data.id;
+
+            var apiUrl = config.apiPath + "/api/Payment/Post";    //mjb
+            var source = {
+                'orderToken': token,
+                'orderName': config.regLoginName,
+                'orderEmail': config.houseName,
+                'orderAmount': cart.getTotalPrice(),
+                'orderHouseId': config.houseId,
+                'ownerId': config.ownerId,
+                'regs': cart.registrations(),
+                'charges': cart.surcharges()
+            };
+			$scope.submitDisabled = true;
+            $http({type: "POST", url: apiUrl, data: source})
+				.success(function (data, status, headers, config) {
+                    var receiptUrl = '#receipt';
+					$scope.submitDisabled = false;
+                    // router.navigateTo(receiptUrl);
+					
+                })
+				.error(function (data, status, headers, config) {
+                    // var respText = JSON.parse(xhr.responseText);
+                    $scope.errorMessage = respText.Message;
+					$scope.submitDisabled = false;
+                    // $("#overlay").addClass("hidden");
+                    // $form.find('button').prop('disabled', false);
+                });
+        };
+		
+        var stripeErrorHandler = function (data, status) {
 
             var $form = $('#payment-form');
-            //alert('in stripe response handle');
-            if (response.error) {
-                alert('error on card ' + response.error.message);
-                // Show the errors on the form
-                //logger.log('error' + response.error.message, null, 'confirm', true);
-                $("#overlay").addClass("hidden");
-                $form.find('.payment-errors').text(response.error.message);
+            alert('error on card ' + data.error.message);
+            // Show the errors on the form
+            //logger.log('error' + response.error.message, null, 'confirm', true);
+            $("#overlay").addClass("hidden");
+            $form.find('.payment-errors').text(response.error.message);
 
-                $form.find('button').prop('disabled', false);
-                //var url = '#receipt';
-                //router.navigateTo(url);
-            } else {
-                //alert('card is approved');
-                //logger.log('card is approved', null, 'confirm', true);
-                $form.find('.payment-errors').text('');  //clears any previous errors
-                // token contains id, last4, and card type
-                var token = response.id;
-                //alert('tok: ' + token);
-                // Insert the token into the form so it gets submitted to the server
-                //$form.append($('<input type="hidden" name="stripeToken" />').val(token));
-                // and re-submit
-                // $form.get(0).submit();
+            $form.find('button').prop('disabled', false);
+            //var url = '#receipt';
+            //router.navigateTo(url);
+        };
+		
+        $scope.completeRegistration = function(){
+            var $form = $('#payment-form');
 
-                //logger.log('lets try to  save reg', null, 'confirm', true);
+			$scope.errorMessage = "";  //clears any previous errors
+			$scope.submitDisabled = true; // Disable the submit button to prevent repeated clicks
 
-                var apiUrl = config.apiPath + "/api/Payment/Post";    //mjb
-                var source = {
-                    'orderToken': token,
-                    'orderName': config.regLoginName,
-                    'orderEmail': config.houseName,
-                    'orderAmount': cart.getTotalPrice(),
-                    'orderHouseId': config.houseId,
-                    'ownerId': config.ownerId,
-                    'regs': cart.registrations(),
-                    'charges': cart.surcharges()
-                };
-                //logger.log('NO error yet', null, 'confirm jonsify', true);
-
-                $.ajax({
-                    type: "POST",
-                    dataType: "json",
-                    url: apiUrl,
-                    data: source,
-                    beforeSend: function (xhr) {
-                        xhr.setRequestHeader("Accept", "application/json");  // explicitly request JSON
-                    },
-                    success: function (result) {
-                        //alert('post returns success' + result);
-                        var receiptUrl = '#receipt';
-                        // router.navigateTo(receiptUrl);
-						
-                    },
-                    error: function (xhr, textStatus, errorThrown) {
-                        //alert('fail' + errorThrown.responseText);
-                        //alert('error4' + textStatus);  //value is error
-                        //alert('error5' + errorThrown);  //valiue is internal server erro
-
-                        var respText = JSON.parse(xhr.responseText);
-                        $form.find('.payment-errors').text(respText.Message);
-                        $("#overlay").addClass("hidden");
-                        $form.find('button').prop('disabled', false);
-                    }
-                });
-            }
+            Stripe.createToken($form)
+				.then(stripeSuccessHandler)
+				.catch(stripeErrorHandler)
+				.finally(function(){
+					// $("#overlay").removeClass("hidden");
+					$scope.submitDisabled = false;
+				});
         };
 
         $scope.isConfirm = function () {
--- app/registration/services/StripeService.js
+++ app/registration/services/StripeService.js
@@ -1,47 +1,30 @@
-(function(){
+(function() {
 
-    function Service($q){
-        //var customerPublishableCode = owner().stripePublishableKey();
+	function Service($q) {
 
-        //This identifies your website in the createToken call below
-        //Stripe.setPublishableKey(customerPublishableCode);
+		var service = {};
 
+		service.setPublishableKey = function(key) {
+			Stripe.setPublishableKey(key)
+		};
 
+		service.createToken = function(form) {
+			console.log("stripe create token form:", form);
+			var deferred = $q.defer();
 
-        //alert('submitting');
-        //var customerPublishableCode = owner().stripePublishableKey();
-        //alert('cpc' + customerPublishableCode);
-        ////This identifies your website in the createToken call below
-        ////Stripe.setPublishableKey('pk_test_bJMgdPZt8B8hINCMgG2vUDy4');
-        //Stripe.setPublishableKey(customerPublishableCode);
-        ////alert('rellay');
-        //$('#payment-form').submit(function (e) {
-        //    var $form = $(this);
+			Stripe.createToken(form, function(status, response) {
+				if (response.error) {
+					deferred.reject(response.error.message, response);
+				} else {
+					deferred.resolve(response.id, response)
+				}
+			});
+			return deferred;
+		};
 
-        //    // Disable the submit button to prevent repeated clicks
-        //    $form.find('button').prop('disabled', true);
+		return service;
+	}
 
-        //    Stripe.createToken($form, stripeResponseHandler);
-
-        //    // Prevent the form from submitting with the default action
-        //    return false;
-        //});
-
-        var service = {};
-
-        service.createToken = function(form) {
-            console.log("form:", form);
-            var response = {
-                error: null,
-                //error.message = "Don't be a dummy!",
-                id: "aweoijh2345oiert5jkadgf"
-            };
-            return $q.when(response.id);
-        };
-
-        return service;
-    }
-
-    angular.module("app").service("StripeService", ["$q", Service]);
+	angular.module("app").service("StripeService", ["$q", Service]);
 
 })();
