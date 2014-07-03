802c1b39bd006e8068dd8f1afe77a733395b46f7
Bill Burke <billstron@gmail.com>
2014-07-03 06:10:50 -0400

    Reverted config

--- app/registration/config.js
+++ app/registration/config.js
@@ -2,97 +2,77 @@
     'use strict';
 
     var app = angular.module('evReg');
-	
-    toastr.options.timeOut = 1000;
-    toastr.options.positionClass = 'toast-bottom-right';
-	
-	// function Config(datacontext){
-		
-		var apiPath = "";
-		apiPath = 'http://evs30api.eventuresports.info';
-	    var remoteServiceName = apiPath + '/breeze/breeze/';
-	    var remoteApiName = apiPath + '/kendo/';
-	
-	    var events = {
-	        controllerActivateSuccess: 'controller.activateSuccess',
-	        spinnerToggle: 'spinner.toggle'
-	    };
 
-	    var imageSettings = {
-	        imageBasePath: '../content/images/photos/',
-	        unknownPersonImageSource: 'unknown_person.jpg'
-	    };
+    // For use with the HotTowel-Angular-Breeze add-on that uses Breeze
+    //var remoteServiceName = 'breeze/Breeze';
+    var remoteServiceName = 'http://evs30api.eventuresports.info/breeze/breeze/';
+    var remoteApiName = 'http://evs30api.eventuresports.info/kendo/';
+    var events = {
+        controllerActivateSuccess: 'controller.activateSuccess',
+        spinnerToggle: 'spinner.toggle'
+    };
 
-	    // var owner = {
-// 	        ownerId: 1,
-// 	        guid: 0,
-// 	        logoImageName: '/Content/images/logo.png',
-//
-// 	        accessType: "none",
-// 	        isReg: false,
-//
-// 	        isHeadfirst: false,
-// 	        isSportsComm: false,
-//
-// 	        wizard: true,
-// 	        wizEventureId: 0,
-// 	        wizEventureListId: 0,
-//
-// 	        isGroupRequired: false,
-//
-// 	        multItemDiscount: false,
-// 	        fourDeLisDiscount: false,
-//
-// 	        isAddSingleFeeForAllRegs: false,
-// 	        addSingleFeeForAllRegsPercent: 0,
-// 	        addSingleFeeType: '',
-// 	        addSingleFeeForAllRegsFlat: 0,
-//
-// 	        eventureName: 'Event',
-// 	        listName: 'List',
-// 	        groupName: 'Group',
-// 	        partButtonText: 'Select PArty!',
-//
-// 	        listStatement: 'Select a desired start time',
-//
-// 	        isEnterpriseDisplayedOnMenu: true,
-// 	        isEventureDisplayedOnMenu: true,
-// 	        isPartDisplayedOnMenu: true,
-// 	        isCouponDisplayedOnMenu: true,
-// 	        isResourceDisplayedOnMenu: true,
-// 	        isReportingDisplayedOnMenu: true
-// 	    };
+    var imageSettings = {
+        imageBasePath: '../content/images/photos/',
+        unknownPersonImageSource: 'unknown_person.jpg'
+    };
 
-		// datacontext.getParticipantById(config.houseId)
-// 			.then(function(part){
-// 				config.participant = part;
-// 			});
-//
-// 		datacontext.getOwnerById(config.ownerId)
-// 			.then(function(owner){
-// 				config.owner = owner;
-// 			});
+    var owner = {
+        ownerId: 1,
+        houseEmail: 0,
+        guid: 0,
+        logoImageName: '/Content/images/logo.png',
 
-	    var config = {
-	        appErrorPrefix: '[evs Error] ', //Configure the exceptionHandler decorator
-	        docTitle: 'eventure sports: ',
-	        events: events,
-			houseId : 13,
-			ownerId : 1,
-	        imageSettings: imageSettings,
-	        version: '3.0.0',
-			apiPath : apiPath,
-	        remoteServiceName: remoteServiceName,
-	        remoteApiName: remoteApiName,
-	        owner: null,
-			participant : null,
-	    };
-		
-		// return config;
-	// }
-	
-    // app.service('config', ["datacontext", Config]);
-	app.value('config', config);
+        houseId: 13,    //this will be set by shell
+        mr_houseId: 0,
+        houseName: "",
+        accessType: "none",
+        isReg: false,
+
+        isHeadfirst: false,
+        isSportsComm: false,
+
+        wizard: true,
+        wizEventureId: 0,
+        wizEventureListId: 0,
+
+        isGroupRequired: false,
+
+        multItemDiscount: false,
+        fourDeLisDiscount: false,
+
+        isAddSingleFeeForAllRegs: false,
+        addSingleFeeForAllRegsPercent: 0,
+        addSingleFeeType: '',
+        addSingleFeeForAllRegsFlat: 0,
+
+        eventureName: 'Event',
+        listName: 'List',
+        groupName: 'Group',
+        partButtonText: 'Select PArty!',
+
+        listStatement: 'Select a desired start time',
+
+        isEnterpriseDisplayedOnMenu: true,
+        isEventureDisplayedOnMenu: true,
+        isPartDisplayedOnMenu: true,
+        isCouponDisplayedOnMenu: true,
+        isResourceDisplayedOnMenu: true,
+        isReportingDisplayedOnMenu: true
+    };
+
+    var config = {
+        appErrorPrefix: '[evs Error] ', //Configure the exceptionHandler decorator
+        docTitle: 'eventure sports: ',
+        events: events,
+        remoteServiceName: remoteServiceName,
+        imageSettings: imageSettings,
+        version: '3.0.0',
+        remoteApiName: remoteApiName,
+        owner: owner
+    };
+
+    app.value('config', config);
 
     app.config(['$logProvider', function ($logProvider) {
         // turn debugging off/on (no info or warn)
