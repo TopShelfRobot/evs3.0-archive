8f22080e9a5e4e70f5a6e1e20a665e600cede8eb
Bill Burke <billstron@gmail.com>
2014-07-15 06:11:27 -0400

    Fixed team payments to move through the process.

--- app/registration/controllers/createteamController.js
+++ app/registration/controllers/createteamController.js
@@ -1,33 +1,33 @@
 (function(){
 
-    var minNameLength = 3;
-    var minTeamSize = 1;
+    var minNameLength = 0;
+    var minTeamSize = 0;
 
-    function controller(scope, $location, cartModel){
+    function controller($scope, $location, cartModel){
 
         console.log("cartModel:", cartModel);
 
-        scope.teamName = "";
-        scope.players = [{name : "", email : ""}];
+        $scope.teamName = "";
+        $scope.players = [{name : "", email : ""}];
 
-        scope.addPlayer = function() {
-            scope.players.push({ name: "", email: "" });
+        $scope.addPlayer = function() {
+            $scope.players.push({ name: "", email: "" });
         };
 
-        scope.makeTeam = function() {
+        $scope.makeTeam = function() {
             var valid = true;
-            cartModel.teamName = scope.teamName || "";
+            cartModel.teamName = $scope.teamName || "";
             if (cartModel.teamName.length < minNameLength) {
                 // make name red
                 valid = false;
             }
             
             cartModel.teamMembers = [];
-            for (var i = 0; i < scope.players.length; i++) {
+            for (var i = 0; i < $scope.players.length; i++) {
                 var name, email;
-                if (scope.players[i].name.length > minNameLength && scope.players[i].email) {
+                if ($scope.players[i].name.length > minNameLength && $scope.players[i].email) {
                     cartModel.teamMembers.push({ name: name, email: email });
-                } else if (scope.players[i].name.length == 0 && scope.players[i].email.length == 0) {
+                } else if ($scope.players[i].name.length == 0 && $scope.players[i].email.length == 0) {
                     // ignore this entry
                     // it's still valid
                 } else {
