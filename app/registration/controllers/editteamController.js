86c2ef0874cdc265b053869b1819a1e1db660ede
Bill Burke <billstron@gmail.com>
2014-08-26 10:17:27 -0400

    tied the team to the ui

--- app/registration/controllers/editteamController.js
+++ app/registration/controllers/editteamController.js
@@ -3,10 +3,19 @@
 	var controllerId = "EditTeamController";
 	function Controller($scope, $location, $routeParams, common, datacontext){
 		
+		var team = null;
 		function getTeam(){
 			return datacontext.team.getTeamById($routeParams.teamId)
 				.then(function(team){
-					$scope.team = team;
+					console.log("Team:", team);
+					$scope.team = {};
+					$scope.team.teamName = team.name;
+					$scope.players = [];
+					var player;
+					for(var i = 0; i < team.teamMembers.length; i++){
+						player = {name: team.teamMembers[i].name, email: team.teamMembers[i].email};
+						$scope.players.push(player);
+					}
 				});
 		}
 		
