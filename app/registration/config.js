e9090b6be40add4a35477106075fdfc090b8d9f6
boone <boone.mike@gmail.com>
2014-10-20 11:32:28 -0400

    switched back to lcfctryout api

--- app/registration/config.js
+++ app/registration/config.js
@@ -7,7 +7,6 @@
     //var remoteServiceName = 'breeze/Breeze';
 	var apiPath = "";
 	 //apiPath = "http://localhost:55972";
-    //apiPath = "http://evs30api.eventuresports.info";
     apiPath = " https://lcfctryouts30Api.eventuresports.com";
 
    
