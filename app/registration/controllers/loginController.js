2caf9d654fd89197f9d4ca7878ed922a1ff0ad19
boone <boone.mike@gmail.com>
2014-11-13 10:36:46 -0500

    adding auth files again

---
+++ app/registration/controllers/loginController.js
@@ -0,0 +1,34 @@
+﻿'use strict';
+(function () {
+
+    var controllerId = "loginController";
+
+    function controller($scope, $location, authService, common) {
+
+        $scope.loginData = {
+            userName: "",
+            password: ""
+        };
+
+        $scope.message = "";
+
+        common.activateController(controllerId);
+
+        $scope.login = function () {
+            
+            authService.login($scope.loginData).then(function (response) {
+                alert('we are logged oin...  go someplace');
+                //$location.path('/orders');
+
+            },
+                 function (err) {
+                     $scope.message = err.error_description;
+                 });
+        };
+      
+    }
+    
+   
+
+    angular.module("evReg").controller(controllerId, ["$scope", "$location", "authService", "common", controller]);
+})();
---
+++ app/registration/services/authInterceptorService.js
@@ -0,0 +1,33 @@
+﻿'use strict';
+angular.module('evReg').factory('authInterceptorService', ['$q', '$location', 'localStorageService', function ($q, $location, localStorageService) {
+
+    //angular.module('evReg').factory('authService', ['$http', '$q', 'localStorageService', 'ngAuthSettings',
+    //function ($http, $q, localStorageService, ngAuthSettings) {
+
+
+    var authInterceptorServiceFactory = {};
+
+    var _request = function (config) {
+
+        config.headers = config.headers || {};
+
+        var authData = localStorageService.get('authorizationData');
+        if (authData) {
+            config.headers.Authorization = 'Bearer ' + authData.token;
+        }
+
+        return config;
+    }
+
+    var _responseError = function (rejection) {
+        if (rejection.status === 401) {
+            $location.path('/login');
+        }
+        return $q.reject(rejection);
+    }
+
+    authInterceptorServiceFactory.request = _request;
+    authInterceptorServiceFactory.responseError = _responseError;
+
+    return authInterceptorServiceFactory;
+}]);
