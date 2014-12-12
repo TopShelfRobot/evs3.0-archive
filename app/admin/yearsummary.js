39cd40d54deeeae26d792baee7fe1b0187e52601
boone <boone.mike@gmail.com>
2014-12-12 17:00:30 -0500

    year summary updates

--- app/admin/yearsummary.js
+++ app/admin/yearsummary.js
@@ -28,17 +28,17 @@
 
         vm.generateChart = function () {
 
-            var chart = vm.summaryChart,
-                        categoryAxis = chart.options.categoryAxis,
-                        baseUnitInputs = $("input:radio[name=baseUnit]");
+            //var chart = vm.summaryChart,
+            //            categoryAxis = chart.options.categoryAxis,
+            //            baseUnitInputs = $("input:radio[name=baseUnit]");
 
-                    categoryAxis.baseUnit = baseUnitInputs.filter(":checked").val();
+            //        categoryAxis.baseUnit = baseUnitInputs.filter(":checked").val();
 
-                    if(categoryAxis.baseUnit === "weeks") {
-                      chart.options.chartArea.height = 1300;
-                    } else {
-                      chart.options.chartArea.height = 475;
-                    }
+            //        if(categoryAxis.baseUnit === "weeks") {
+            //          chart.options.chartArea.height = 1300;
+            //        } else {
+            //          chart.options.chartArea.height = 475;
+            //        }
 
                     chart.refresh();
 
@@ -59,64 +59,71 @@
             var title = "Number of Registrations";
             //var yearapi = config.remoteApiName + 'Registrations/GetYearOverYearData/1'; //+ vm.ownerId;
 
-            var stats = [
-                    { value: 1, date: new Date("01/01/2013") },
-                    { value: 2, date: new Date("05/08/2013") },
-                    { value: 3, date: new Date("08/15/2013") },
-                    { value: 4, date: new Date("09/01/2013") },
-                    { value: 5, date: new Date("10/08/2013") },
-                    { value: 6, date: new Date("11/15/2013") },
-                    { value: 7, date: new Date("12/22/2013") },
-                    { value: 7, date: new Date("05/08/2014") },
-                    { value: 6, date: new Date("08/15/2014") },
-                    { value: 5, date: new Date("09/01/2014") },
-                    { value: 4, date: new Date("10/08/2014") }
-                ];
+            //var stats = [
+            //        { value: 1, date: new Date("01/01/2013") },
+            //        { value: 2, date: new Date("05/08/2013") },
+            //        { value: 3, date: new Date("08/15/2013") },
+            //        { value: 4, date: new Date("09/01/2013") },
+            //        { value: 5, date: new Date("10/08/2013") },
+            //        { value: 6, date: new Date("11/15/2013") },
+            //        { value: 7, date: new Date("12/22/2013") },
+            //        { value: 7, date: new Date("05/08/2014") },
+            //        { value: 6, date: new Date("08/15/2014") },
+            //        { value: 5, date: new Date("09/01/2014") },
+            //        { value: 4, date: new Date("10/08/2014") }
+            //    ];
 
-            var yearOverYearSeries = [{
-                    field: "value",
-                    name: "Event",
-                    categoryField: "date"
-                }
-                ];
+            //var yearOverYearSeries = [{
+            //        field: "value",
+            //        name: "Event",
+            //        categoryField: "date"
+            //    }
+             //    ];
+
+            var yearapi = config.remoteApiName + 'analytic/GetYearOverYearData/1/1/2013/2014';//  + vm.ownerId; + "/" +
 
             vm.yearOverYear = {
-                theme: "bootstrap",
-                // dataSource: {
-                //     transport: {
-                //         read: yearapi,
-                //         dataType: "json"
-                //     }
-                // },
-                dataSource: {
-                            data: stats
-                        },
-                title: {
-                    text: title
-                },
+                theme: "material",
+                 dataSource: {
+                     transport: {
+                         read: yearapi,
+                         dataType: "json"
+                     },
+                     group: {
+                        field: "year"
+                     },
+                     sort: {
+                         field: "month",
+                         dir: "asc"
+                     },
+                     schema: {
+                         model: {
+                             fields: {
+                                 month: {
+                                     type: "date"
+                                 }
+                             }
+                         }
+                     }
+                 },
+                 
                 legend: {
                     position: "bottom"
                 },
-                seriesDefaults: {
-                    type: "bar",
-                    labels: {
-                        visible: true,
-                        background: "transparent"
-                    }
-                },
-                chartArea: {
-                  height: 475
-                },
-                series: yearOverYearSeries,
+                series: [{
+                    type: "column",
+                    field: "registrations"   //,
+                    //name: "#= group.value # (close)"   //tool tips
+                }],
                 valueAxis: {
                     line: {
                         visible: false
                     }
                 },
                 categoryAxis: {
-                    baseUnit: "months",
-                    majorGridLines: {
-                        visible: false
+                    field: "month",
+                    labels: {
+                        format: "MMM"
                     }
                 }
             };
