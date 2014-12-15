d66db0e4f1e3183c7e820262039ab60c71caf2e8
Wil Goolsby <wgoolsby@me.com>
2014-12-15 11:10:14 -0500

    Fix mixing var on yoy

--- app/admin/yearsummary.js
+++ app/admin/yearsummary.js
@@ -47,13 +47,16 @@
         }
 
         vm.generateChart = function () {
+
+            var chart = vm.summaryChart;
+
             var dataSource = new kendo.data.DataSource({
                  transport: {
                      read: {
                          url: function () {
                              return config.remoteApiName + 'analytic/GetYearOverYearData/' + vm.ownerId + '/' + vm.chart.eventure  + '/' + vm.chart.startYear  + '/' + vm.chart.endYear  + '/' + vm.chart.type;
                          },
-                         dataType: 'json'
+                         dataType: "json"
                      }
                  }
             });
