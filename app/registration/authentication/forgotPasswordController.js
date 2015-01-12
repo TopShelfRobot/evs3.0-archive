7affdded2f0be73054c7ab77158ab6d86dec6ef8
Wil Goolsby <wgoolsby@me.com>
2015-01-12 14:35:06 -0500

    handle user not found in forgot password request

--- app/registration/authentication/forgotPasswordController.js
+++ app/registration/authentication/forgotPasswordController.js
@@ -17,8 +17,18 @@
 
         $scope.requestPassword = function () {
 
-            authService.forgotPassword($scope.registration);
-
+            authService.forgotPassword($scope.registration).then(function(response) {
+                    $scope.message = 'Please check your email. A link has been provided.';
+            },
+            function (response) {
+               var errors = [];
+               for (var key in response.data.modelState) {
+                    for (var i = 0; i < response.data.modelState[key].length; i++) {
+                        errors.push(response.data.modelState[key][i]);
+                    }
+               }
+               $scope.message = 'Sorry. This user name was not found.';
+        });
 
 
         };
