fd79411096cc4dec7780b5b4f1a4f7753c43fa63
Wil Goolsby <wgoolsby@me.com>
2015-01-12 14:44:59 -0500

    Set apiPath to root

--- app/dashboard/config.js
+++ app/dashboard/config.js
@@ -9,7 +9,7 @@
 	
 	var apiPath = '';
 	//apiPath = 'http://localhost:49822/';
-	apiPath = 'http://dev30.eventuresports.info/';
+	//apiPath = 'http://dev30.eventuresports.info/';
 	
 	var remoteServiceName = apiPath + 'bdc/dashboard/';
 	var remoteApiName = apiPath + 'api/';
