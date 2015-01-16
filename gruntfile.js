38274eec6854b821ccfd752966549b353a39e16a
Wil Goolsby <wgoolsby@me.com>
2015-01-15 20:19:49 -0500

    fix messed up .jshintrc

--- gruntfile.js
+++ gruntfile.js
@@ -1,24 +1,24 @@
-module.exports = function(grunt) {
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-// Sass config.
-        sass: {
-            dist: {
-                options: {
-                    style: 'compressed'
-                },
-                files: {
-                    'Content/rawcss/custom-colors.css': 'scss/custom-colors.scss',
-                    'Content/rawcss/dashboard.css': 'scss/dashboard.scss',
-                    'Content/rawcss/registration.css': 'scss/registration.scss'
-                }
-            }
-        },
-//CSS Min config
-        cssmin: {
-            target: {
-                files: {
-                    'Content/rawcss/framework.css': ['Content/rawcss/kendo.common.min.css',
+module.exports = function (grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		// Sass config.
+		sass: {
+			dist: {
+				options: {
+					style: 'compressed'
+				},
+				files: {
+					'Content/rawcss/custom-colors.css': 'scss/custom-colors.scss',
+					'Content/rawcss/dashboard.css': 'scss/dashboard.scss',
+					'Content/rawcss/registration.css': 'scss/registration.scss'
+				}
+			}
+		},
+		//CSS Min config
+		cssmin: {
+			target: {
+				files: {
+					'Content/rawcss/framework.css': ['Content/rawcss/kendo.common.min.css',
                         'Content/rawcss/kendo.common-material.core.min.css',
                         'Content/rawcss/kendo.common-material.min.css',
                         'Content/rawcss/kendo.material.min.css',
@@ -33,17 +33,17 @@ module.exports = function(grunt) {
                         'Content/rawcss/nsPopover.custom.css',
                         'Content/rawcss/font-awesome.min.css',
                         'Content/rawcss/social-buttons.css'],
-                    'Content/css/reg-style.css': ['Content/rawcss/framework.css',
+					'Content/css/reg-style.css': ['Content/rawcss/framework.css',
                         'Content/registration.css'],
-                    'Content/css/dash-style.css': ['Content/rawcss/framework.css',
+					'Content/css/dash-style.css': ['Content/rawcss/framework.css',
                         'Content/dashboard.css']
-                }
-            }
-        }
-    });
-// this is where you say, hey, I'm using that sass thing that I just created settings for.
-    grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-// this is where you have Grunt compile your sass when you type "grunt" into the terminal
-    grunt.registerTask('default', ['sass']);
+				}
+			}
+		}
+	});
+	// this is where you say, hey, I'm using that sass thing that I just created settings for.
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	// this is where you have Grunt compile your sass when you type "grunt" into the terminal
+	grunt.registerTask('default', ['sass']);
 };
