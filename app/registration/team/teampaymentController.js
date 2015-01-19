60a8721a09ea84e085b3d7d7bf812e0b3414e26c
Wil Goolsby <wgoolsby@me.com>
2015-01-19 15:12:25 -0500

    Team Payment

--- app/registration/team/teampaymentController.js
+++ app/registration/team/teampaymentController.js
@@ -1,5 +1,5 @@
 (function () {
-	
+
 	var controllerId = "TeamPaymentController";
 
 	function controller($scope, $location, $http, $modal, datacontext, cartModel, config, stripe, common) {
@@ -16,44 +16,43 @@
 		var promises = [
 			datacontext.eventure.getEventureListById(cartModel.eventureListId)
 				.then(function (item) {
-					if (item) {
-						$scope.fee = item.currentFee;
-						$scope.paymentTerms = item.paymentTerms;
-						console.log(item.paymentTerms);
-						cartModel.fee = item.currentFee;
-						//alert('itemlistingtype: ' + item.eventureListTypeId + 'fee: ' + item.currentFee)
-						switch (item.eventureListType) {
-							case "TeamSponsored":    //team sponsor
-								$scope.isIndividualVisible = true;
-								$scope.userPaying = item.currentFee;
-								break;
-							case "TeamSuggest":   //team suggest
-							    $scope.isSuggestPayVisible = true;
-							    $scope.userPaying = 0;
-							
-								break;
-							case "TeamIndividual":    //team all pays the same
-								$scope.isIndividualVisible = true;
-								$scope.userPaying = item.currentFee;
-								break;
-							case "Lottery":    //Captain pays all or nothing at registration
-								$scope.isLotteryVisible = true;
-								$scope.userPaying = item.currentFee;
-								break;
-							default:
-						}
-						//if (item.listingType == 2) {
-						//    $scope.isIndividualVisible = false;
-						//}
-						//if (item.listingType == 3) {
-						//    $scope.isSuggestPayVisible = true;
-						//}
+				if (item) {
+					$scope.fee = item.currentFee;
+					$scope.paymentTerms = item.paymentTerms;
+					console.log(item.paymentTerms);
+					cartModel.fee = item.currentFee;
+					//alert('itemlistingtype: ' + item.eventureListTypeId + 'fee: ' + item.currentFee)
+					switch (item.eventureListType) {
+					case "TeamSponsored": //team sponsor
+						$scope.isIndividualVisible = true;
+						$scope.userPaying = item.currentFee;
+						break;
+					case "TeamSuggest": //team suggest
+						$scope.isSuggestPayVisible = true;
+						$scope.userPaying = 0;
+						break;
+					case "TeamIndividual": //team all pays the same
+						$scope.isIndividualVisible = true;
+						$scope.userPaying = item.currentFee;
+						break;
+					case "Lottery": //Captain pays all or nothing at registration
+						$scope.isLotteryVisible = true;
+						$scope.userPaying = item.currentFee;
+						break;
+					default:
 					}
-				})
+					//if (item.listingType == 2) {
+					//    $scope.isIndividualVisible = false;
+					//}
+					//if (item.listingType == 3) {
+					//    $scope.isSuggestPayVisible = true;
+					//}
+				}
+			})
 		];
-		
+
 		common.activateController(promises, controllerId);
-		
+
 		$scope.allowZeroPayment = cartModel.allowZeroPayment;
 		// $scope.waiverSigned = cartModel.waiverSigned;
 
@@ -67,68 +66,63 @@
 				controller: 'TermsModalInstance'
 			});
 
-			modalInstance.result.then(function () { $scope.checkout(); });
+			modalInstance.result.then(function () {
+				$scope.checkout();
+			});
 
 		};
 
 		$scope.checkout = function () {
+
 			var cartOrder = cartModel.order($scope.userPaying);
 
-			//var order = {
-			//    'orderAmount': $scope.userPaying,     //cart.getTotalPrice(),
-			//    'orderHouseId': config.owner.houseId,
-			//    'ownerId': 1,
-			//    'regs': cartOrder.regs
-			//    //'charges': cart.surcharges
-			//};
-
-		    //$.blockUI({ message: 'Processing order...' });
-
-			console.log($scope.userPaying);
-
-			if ($scope.userPaying > 0) {
-			    stripe.checkout(cartOrder.orderAmount)
-                    .then(function (res) {
-                        console.log(res);
-                        $.blockUI({ message: 'Processing order...' });
-                        cartOrder.stripeToken = res.id;
-                        $http.post(config.apiPath + "api/payment/PostTeam", cartOrder)
-                        //$http.post(config.apiPath + "api/payment/PostTeamPayment/testet")
-                            .success(function (result) {
-                                console.log("result: " + result);
-                                $location.path("/receipt/" + result);
-                            })
-                            .error(function (err) {
-                                console.error("ERROR:", err.toString());
-                            })
-                            .finally(function () {
-                                $.unblockUI();
-                            });
-                    });
-			}
-			else
-			{
-			    //console.log(res);
+			if ($scope.isUserPaying === true) {
+				alert('User elected to pay.: ' + $scope.userPaying);
+				// Process payment as usual
+				stripe.checkout(cartOrder.orderAmount)
+					.then(function (res) {
+						console.log(res);
+						$.blockUI({
+							message: 'Processing order...'
+						});
+						cartOrder.stripeToken = res.id;
+						$http.post(config.apiPath + "api/payment/PostTeam", cartOrder)
+							.success(function (result) {
+								console.log("result: " + result);
+								$location.path("/receipt/" + result);
+							})
+							.error(function (err) {
+								console.error("ERROR:", err.toString());
+							})
+							.finally(function () {
+								$.unblockUI();
+							});
+					});
+			} else {
+				//Set userPaying to 0
+				//Process Team Registration
 				$scope.userPaying = 0;
-			    $.blockUI({ message: 'Processing order...' });
-			   // cartOrder.stripeToken = res.id;
-			    $http.post(config.apiPath + "api/payment/PostTeam", cartOrder)
-                //$http.post(config.apiPath + "api/payment/PostTeamPayment/testet")
-                    .success(function (result) {
-                        console.log("result: " + result);
-                        $location.path("/receipt/" + result);
-                    })
-                    .error(function (err) {
-                        console.error("ERROR:", err.toString());
-                    })
-                    .finally(function () {
-                        $.unblockUI();
-                    });
+				alert('User elected to not pay: ' + $scope.userPaying);
+				$.blockUI({
+					message: 'Processing order...'
+				});
+				// cartOrder.stripeToken = res.id;
+				$http.post(config.apiPath + "api/payment/PostTeam", cartOrder)
+					.success(function (result) {
+						console.log("result: " + result);
+						$location.path("/receipt/" + result);
+					})
+					.error(function (err) {
+						console.error("ERROR:", err.toString());
+					})
+					.finally(function () {
+						$.unblockUI();
+					});
 			}
+
 		};
 	}
-	
-	angular.module("evReg").controller(controllerId,
-		["$scope", "$location", "$http", "$modal", "datacontext", "RegistrationCartModel", "config","StripeService", "common", controller]);
-	
+
+	angular.module("evReg").controller(controllerId, ["$scope", "$location", "$http", "$modal", "datacontext", "RegistrationCartModel", "config", "StripeService", "common", controller]);
+
 })();
