67c105a2a75f79c34a96741f26a83814016237bb
Wil Goolsby <wgoolsby@me.com>
2015-02-03 11:08:30 -0500

    remove angular file upload for Boones solution

--- app/dashboard/app.js
+++ app/dashboard/app.js
@@ -19,7 +19,6 @@
 		'ui.bootstrap', // ui-bootstrap (ex: carousel, pagination, dialog)
 		'kendo.directives', // kendo-angular (grid, dataviz)
 		'angularMoment', // Date and Time Format
-		'angularFileUpload',  // file upload functions
 		'angulartics',  //analytics
 		'angulartics.google.analytics', //analytics
 		'evReg',
--- app/dashboard/eventure/seteventure.js
+++ app/dashboard/eventure/seteventure.js
@@ -2,9 +2,9 @@
 	'use strict';
 
 	var controllerId = 'seteventure';
-	angular.module('app').controller(controllerId, ['$routeParams', '$upload', '$timeout', '$location', '$scope', 'common', 'datacontext', seteventure]);
+	angular.module('app').controller(controllerId, ['$routeParams', '$timeout', '$location', '$scope', 'common', 'datacontext', seteventure]);
 
-	function seteventure($routeParams, $upload, $timeout, $location, $scope, common, datacontext) {
+	function seteventure($routeParams, $timeout, $location, $scope, common, datacontext) {
 
 		var getLogFn = common.logger.getLogFn;
 		var log = getLogFn(controllerId);
@@ -38,7 +38,7 @@
 				return vm.eventure = datacontext.eventure.createEventure();
 			}
 		}
-      
+
         vm.cancel = function() {
 			$location.path("/eventurecenter");
         };
@@ -84,174 +84,5 @@
 
 		vm.format = vm.formats[0];
 
-		//File Upload
-		vm.fileReaderSupported = window.FileReader !== null;
-		vm.uploadRightAway = true;
-		vm.changeAngularVersion = function() {
-			window.location.hash = vm.angularVersion;
-			window.location.reload(true);
-		};
-		vm.hasUploader = function(index) {
-			return vm.upload[index] !== null;
-		};
-		vm.abort = function(index) {
-			vm.upload[index].abort();
-			vm.upload[index] = null;
-		};
-		vm.angularVersion = window.location.hash.length > 1 ? window.location.hash.substring(1) : '1.2.0';
-		vm.onFileSelect = function($files) {
-			vm.selectedFiles = [];
-			vm.progress = [];
-			if (vm.upload && vm.upload.length > 0) {
-				for (var i = 0; i < vm.upload.length; i++) {
-					if (vm.upload[i] !== null) {
-						vm.upload[i].abort();
-					}
-				}
-			}
-			vm.upload = [];
-			vm.uploadResult = [];
-			vm.selectedFiles = $files;
-			vm.dataUrls = [];
-			for (var i = 0; i < $files.length; i++) {
-				var $file = $files[i];
-				if (window.FileReader && $file.type.indexOf('image') > -1) {
-					var fileReader = new FileReader();
-					fileReader.readAsDataURL($files[i]);
-					var loadFile = function(fileReader, index) {
-						fileReader.onload = function(e) {
-							$timeout(function() {
-								vm.dataUrls[index] = e.target.result;
-							});
-						};
-					}(fileReader, i);
-				}
-				vm.progress[i] = -1;
-				if (vm.uploadRightAway) {
-					vm.start(i);
-				}
-			}
-		};
-
-		vm.start = function(index) {
-			vm.progress[index] = 0;
-			vm.errorMsg = null;
-			if (vm.howToSend == 1) {
-				vm.upload[index] = $upload.upload({
-					url: '/Content/images',
-					method: PUT,
-					headers: {
-						'my-header': 'my-header-value'
-					},
-					data: {
-						myModel: vm.myModel
-					},
-					/* formDataAppender: function(fd, key, val) {
-                  if (angular.isArray(val)) {
-                                angular.forEach(val, function(v) {
-                                  fd.append(key, v);
-                                });
-                              } else {
-                                fd.append(key, val);
-                              }
-                }, */
-					/* transformRequest: [function(val, h) {
-                  console.log(val, h('my-header')); return val + 'aaaaa';
-                }], */
-					file: vm.selectedFiles[index],
-					fileFormDataName: 'myFile'
-				}).then(function(response) {
-					vm.uploadResult.push(response.data);
-				}, function(response) {
-					if (response.status > 0) vm.errorMsg = response.status + ': ' + response.data;
-				}, function(evt) {
-					// Math.min is to fix IE which reports 200% sometimes
-					vm.progress[index] = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
-				}).xhr(function(xhr) {
-					xhr.upload.addEventListener('abort', function() {
-						console.log('abort complete')
-					}, false);
-				});
-			} else {
-				var fileReader = new FileReader();
-				fileReader.onload = function(e) {
-					vm.upload[index] = $upload.http({
-						url: '/Content/images',
-						headers: {
-							'Content-Type': vm.selectedFiles[index].type
-						},
-						data: e.target.result
-					}).then(function(response) {
-						vm.uploadResult.push(response.data);
-					}, function(response) {
-						if (response.status > 0) vm.errorMsg = response.status + ': ' + response.data;
-					}, function(evt) {
-						// Math.min is to fix IE which reports 200% sometimes
-						vm.progress[index] = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
-					});
-				};
-				fileReader.readAsArrayBuffer(vm.selectedFiles[index]);
-			}
-		};
-
-		vm.resetInputFile = function() {
-			var elems = document.getElementsByTagName('input');
-			for (var i = 0; i < elems.length; i++) {
-				if (elems[i].type == 'file') {
-					elems[i].value = null;
-				}
-			}
-		};
-
 	}
 })();
-
-//define(['services/logger', 'services/datacontext', 'durandal/plugins/router', 'config'],  //, 'viewmodels/shared/debug'
-
-//    function (logger, datacontext, router, config) {
-
-//        var eventure = ko.observable();
-//        var eventureId;
-
-//        var activate = function (routeData) {
-//            logger.log('test activating.', null, 'ec', true);
-
-//            //$('#header').addClass('hidden');
-
-//            eventureId = parseInt(routeData.id);
-//            logger.log('test activating.' + eventureId, null, 'ec', true);
-//            if (isNaN(eventureId))   //
-//                config.wizard = true;     //we took this page out of the wizard but wizard=true still indicates a new event
-//            else
-//                config.wizard = false;
-
-//            if (config.wizard)
-//                return eventure(datacontext.createEventure());
-//            else
-//                return datacontext.getEventureById(eventureId, eventure);  //mjb this is wrong should be id passed in
-
-//        };
-
-
-//        //var clickNext = function () {
-//        //    //logger.log('next', null, 'seteventure', true);
-//        //    eventure().ownerId(config.ownerId);
-
-//        //    var form = $(".form-horizontal");
-//        //    form.validate();
-
-//        //    if (form.valid())
-//        //        saveAndNav();
-//        //};
-
-
-
-
-//        var vm = {
-//            activate: activate,
-//            eventure: eventure,
-//            clickNext: clickNext,
-//            viewAttached: viewAttached
-//        };
-//        return vm;
-//    });
