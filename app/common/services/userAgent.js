daf3cbfce5e7779df12a72c5d025c7faf09b8553
boone <boone.mike@gmail.com>
2015-02-09 16:23:30 -0500

    new userAgent.js file

---
+++ app/common/services/userAgent.js
@@ -0,0 +1,56 @@
+﻿; (function () {
+
+    function Service(http, datacontext) {
+        this.logAgentInfo = function (participant) {
+            //var today = new Date();
+            //var birthDate = new Date(dateString);
+            //var age = today.getFullYear() - birthDate.getFullYear();
+            //var m = today.getMonth() - birthDate.getMonth();
+            //if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+            //    age--;
+            //}
+            //return age;
+
+
+            //var logText = "<p>Browser CodeName: " + navigator.appCodeName + "</p>";
+            //logText += "<p>Browser Name: " + navigator.appName + "</p>";
+            //logText += "<p>Browser Version: " + navigator.appVersion + "</p>";
+            //logText += "<p>Cookies Enabled: " + navigator.cookieEnabled + "</p>";
+            //logText += "<p>Platform: " + navigator.platform + "</p>";
+            //logText += "<p>User-agent header: " + navigator.userAgent + "</p>";
+            //logText += "<p>User-agent language: " + navigator.systemLanguage + "</p>";
+            //alert(logText);
+            var vm = this;
+
+
+            vm.userAgent = datacontext.participant.createUserAgent();
+
+            vm.appCodeName = navigator.appName;
+            vm.appVersion = navigator.appVersion
+            vm.cookieEnabled = navigator.cookieEnabled
+            vm.platform = navigator.platform
+            vm.userAgent = navigator.userAgent
+            vm.systemLanguage = navigator.systemLanguage
+            
+
+            $http.jsonp('http://ipinfo.io/?callback=JSON_CALLBACK')
+                .success(function (data) {
+                    vm.ip = data.ip;
+                    vm.hostname = data.hostname;
+                    vm.loc = data.loc; //Latitude and Longitude
+                    vm.org = data.org; //organization
+                    vm.city = data.city;
+                    vm.region = data.region; //state
+                    vm.country = data.country;
+                    vm.phone = data.phone; //city area code
+                }).error(function () {
+                    console.log('no ip info');
+                }).finally(function () {
+                    //save changes
+                });
+
+        };
+    }
+
+    angular.module("common").service("UserAgent",['$http', 'datacontext', Service]);
+})();
