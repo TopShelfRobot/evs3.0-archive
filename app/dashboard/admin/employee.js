dbad24157d7e554f1da91d84f68b697859a9791d
Wil Goolsby <wgoolsby@me.com>
2015-02-19 14:33:38 -0500

    formatting fix employee.js

--- app/dashboard/admin/employee.js
+++ app/dashboard/admin/employee.js
@@ -8,13 +8,15 @@
 			vm.selectedUser = null;
 			if (vm.employeeEmail) {
 				// search by email
-				datacontext.participant.getParticipantsBySearchingEmail(vm.employeeEmail) //TODO whg needs to be changed to employee
+				//TODO whg needs to be changed to employee
+				datacontext.participant.getParticipantsBySearchingEmail(vm.employeeEmail)
 					.then(function (list) {
 						vm.searchResults = list;
 					});
 			} else if (vm.partName) {
 				// search by name
-				datacontext.participant.getParticipantsBySearchingName(vm.employeeName) //TODO whg needs to be changed to employee
+				//TODO whg needs to be changed to employee
+				datacontext.participant.getParticipantsBySearchingName(vm.employeeName)
 					.then(function (list) {
 						vm.searchResults = list;
 					});
@@ -30,13 +32,13 @@
 		vm.selectedUser = null;
 
 		vm.createNewUser = function () {
-			$location.path("/user-profile/add"); //TODO whg Should probably be a separate page for employee add
+			$location.path('/user-profile/add'); //TODO whg Should probably be a separate page for employee add
 		};
 
 		if (config.owner.newId && config.owner.houseId) {
 			console.log(config);
 			config.owner.newId = false;
-			$location.path("/eventure/");
+			$location.path('/eventure/');
 		}
 	}
 
