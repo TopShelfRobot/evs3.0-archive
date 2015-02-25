4da9cfd7c2e686b1482b7d1b389aa2627d1770a3
Wil Goolsby <wgoolsby@me.com>
2015-02-25 15:55:55 -0500

    latest requested changes

--- app/registration/team/teampaymentController.js
+++ app/registration/team/teampaymentController.js
@@ -1,132 +1,131 @@
 (function () {
 
-	var controllerId = "TeamPaymentController";
+  var controllerId = "TeamPaymentController";
 
-	function controller($scope, $location, $http, $modal,
-		datacontext, cartModel, cartRegSettings, config, stripe, common) {
-		$scope.teamName = cartModel.teamName;
-		$scope.waiverSigned = false;
-		$scope.isSuggestPayVisible = false;
-		$scope.isIndividualVisible = false;
-		$scope.isLotteryVisible = false;
+  function controller($scope, $location, $http, $modal,
+    datacontext, cartModel, cartRegSettings, config, stripe, common) {
+    $scope.teamName = cartModel.teamName;
+    $scope.waiverSigned = false;
+    $scope.isSuggestPayVisible = false;
+    $scope.isIndividualVisible = false;
+    $scope.isLotteryVisible = false;
 
-		$scope.isUserPaying = false;
+    $scope.isUserPaying = false;
 
-		$scope.confirmButtonText = cartModel.confirmButtonText;
+    $scope.confirmButtonText = cartModel.confirmButtonText;
 
-		var promises = [
-			datacontext.eventure.getEventureListById(cartModel.eventureListId)
-				.then(function (item) {
-				if (item) {
-					$scope.fee = item.currentFee;
-					$scope.paymentTerms = item.paymentTerms;
-					console.log(item.paymentTerms);
-					cartModel.fee = item.currentFee;
-					//alert('itemlistingtype: ' + item.eventureListTypeId + 'fee: ' + item.currentFee)
-					switch (item.eventureListType) {
-					case "TeamSponsored": //team sponsor
-						$scope.isIndividualVisible = true;
-						$scope.userPaying = item.currentFee;
-						break;
-					case "TeamSuggest": //team suggest
-						$scope.isSuggestPayVisible = true;
-						$scope.userPaying = 0;
-						break;
-					case "TeamIndividual": //team all pays the same
-						$scope.isIndividualVisible = true;
-						$scope.userPaying = item.currentFee;
-						break;
-					case "Lottery": //Captain pays all or nothing at registration
-						$scope.isLotteryVisible = true;
-						$scope.userPaying = item.currentFee;
-						break;
-					default:
-					}
-					//if (item.listingType == 2) {
-					//    $scope.isIndividualVisible = false;
-					//}
-					//if (item.listingType == 3) {
-					//    $scope.isSuggestPayVisible = true;
-					//}
-				}
-			})
-		];
+    var promises = [
+   datacontext.eventure.getEventureListById(cartModel.eventureListId)
+    .then(function (item) {
+        if (item) {
+          $scope.fee = item.currentFee;
+          $scope.paymentTerms = item.paymentTerms;
+          console.log(item.paymentTerms);
+          cartModel.fee = item.currentFee;
+          //alert('itemlistingtype: ' + item.eventureListTypeId + 'fee: ' + item.currentFee)
+          switch (item.eventureListType) {
+          case "TeamSponsored": //team sponsor
+            $scope.isIndividualVisible = true;
+            $scope.userPaying = item.currentFee;
+            break;
+          case "TeamSuggest": //team suggest
+            $scope.isSuggestPayVisible = true;
+            $scope.userPaying = 0;
+            break;
+          case "TeamIndividual": //team all pays the same
+            $scope.isIndividualVisible = true;
+            $scope.userPaying = item.currentFee;
+            break;
+          case "Lottery": //Captain pays all or nothing at registration
+            $scope.isLotteryVisible = true;
+            $scope.userPaying = item.currentFee;
+            break;
+          default:
+          }
+          //if (item.listingType == 2) {
+          //    $scope.isIndividualVisible = false;
+          //}
+          //if (item.listingType == 3) {
+          //    $scope.isSuggestPayVisible = true;
+          //}
+        }
+      })
+  ];
 
-		common.activateController(promises, controllerId);
+    common.activateController(promises, controllerId);
 
-		$scope.allowZeroPayment = cartModel.allowZeroPayment;
-		// $scope.waiverSigned = cartModel.waiverSigned;
+    $scope.allowZeroPayment = cartModel.allowZeroPayment;
+    // $scope.waiverSigned = cartModel.waiverSigned;
 
-		$scope.errorMessage = "";
+    $scope.errorMessage = "";
 
-		$scope.open = function () {
-			var modalInstance = $modal.open({
-				templateUrl: 'termsAndConditions.html',
-				size: 'lg',
-				backdrop: 'static',
-				controller: 'TermsModalInstance'
-			});
+    $scope.open = function () {
+      var modalInstance = $modal.open({
+        templateUrl: 'termsAndConditions.html',
+        size: 'lg',
+        backdrop: 'static',
+        controller: 'TermsModalInstance'
+      });
 
-			modalInstance.result.then(function () {
-				$scope.checkout();
-			});
+      modalInstance.result.then(function () {
+        $scope.checkout();
+      });
 
-		};
+    };
 
-		$scope.checkout = function () {
+    $scope.checkout = function () {
 
-			var cartOrder = cartModel.order($scope.userPaying);
+      var cartOrder = cartModel.order($scope.userPaying);
 
-			if ($scope.isUserPaying === true) {
-				console.log('User elected to pay.: ' + cartOrder.orderAmount);
-				// Process payment as usual
-				stripe.checkout(cartOrder.orderAmount)
-					.then(function (res) {
-						console.log(res);
-						$.blockUI({
-							message: 'Processing order...'
-						});
-						cartOrder.stripeToken = res.id;
-						$http.post(config.apiPath + "api/payment/PostTeam", cartOrder)
-							.success(function (result) {
-								console.log("result: " + result);
-								$location.path("/receipt/" + result);
-							})
-							.error(function (err) {
-								console.error("ERROR:", err.toString());
-							})
-							.finally(function () {
-								$.unblockUI();
-							});
-					});
-			} else {
-				//Set userPaying to 0
-				//Process Team Registration
-				cartOrder.orderAmount = 0;
-				console.log('User elected to not pay: ' + cartOrder.orderAmount);
-				$.blockUI({
-					message: 'Processing order...'
-				});
-				// cartOrder.stripeToken = res.id;
-				$http.post(config.apiPath + "api/payment/PostTeam", cartOrder)
-					.success(function (result) {
-						console.log("result: " + result);
-						$location.path("/receipt/" + result);
-					})
-					.error(function (err) {
-						console.error("ERROR:", err.toString());
-					})
-					.finally(function () {
-						$.unblockUI();
-					});
-			}
+      if ($scope.isUserPaying) {
+        console.log('User elected to pay.: ' + cartOrder.orderAmount);
+        // Process payment as usual
+        stripe.checkout(cartOrder.orderAmount)
+          .then(function (res) {
+            console.log(res);
+            $.blockUI({
+              message: 'Processing order...'
+            });
+            cartOrder.stripeToken = res.id;
+            $http.post(config.apiPath + "api/payment/PostTeam", cartOrder)
+              .success(function (result) {
+                console.log("result: " + result);
+                $location.path("/receipt/" + result);
+              })
+              .error(function (err) {
+                console.error("ERROR:", err.toString());
+              })
+              .finally(function () {
+                $.unblockUI();
+              });
+          });
+      } else {
+        //Set userPaying to 0
+        //Process Team Registration
+        cartOrder.orderAmount = 0;
+        console.log('User elected to not pay: ' + cartOrder.orderAmount);
+        $.blockUI({
+          message: 'Processing order...'
+        });
+        // cartOrder.stripeToken = res.id;
+        $http.post(config.apiPath + "api/payment/PostTeam", cartOrder)
+          .success(function (result) {
+            console.log("result: " + result);
+            $location.path("/receipt/" + result);
+          })
+          .error(function (err) {
+            console.error("ERROR:", err.toString());
+          })
+          .finally(function () {
+            $.unblockUI();
+          });
+      }
 
-		};
-	}
+    };
+  }
 
-	angular.module("evReg").controller(controllerId,
-		["$scope", "$location", "$http", "$modal",
-		"datacontext", "RegistrationCartModel", "CartModel",
-		"config", "StripeService", "common", controller]);
+  angular.module("evReg").controller(controllerId, ["$scope", "$location", "$http", "$modal",
+  "datacontext", "RegistrationCartModel", "CartModel",
+  "config", "StripeService", "common", controller]);
 
 })();
