f8c90fa18b149fced6ee6ff55c78de74fe97669c
Wil Goolsby <wgoolsby@me.com>
2015-02-26 14:20:00 -0500

    Add division and timeFinish to cartModel

--- app/registration/team/createteamController.js
+++ app/registration/team/createteamController.js
@@ -1,64 +1,78 @@
-(function(){
+(function () {
 
-    var minNameLength = 0;
-    var minTeamSize = 0;
-	
-	var controllerId = "CreateTeamController";
+  var minNameLength = 0;
+  var minTeamSize = 0;
 
-    function controller($scope, $location, $routeParams, cartModel, common){
+  var controllerId = "CreateTeamController";
 
-        console.log("cartModel:", cartModel);
+  function controller($scope, $location, $routeParams, cartModel, common) {
 
-		cartModel.eventureId = $routeParams.eventureId;
-		cartModel.eventureListId = $routeParams.listId;
-		cartModel.participantId = $location.search()["uid"];
+    console.log("cartModel:", cartModel);
 
-        $scope.team = {};
-        $scope.team.teamName = "";
-        $scope.players = [{name : "", email : "", position: ""}];
+    cartModel.eventureId = $routeParams.eventureId;
+    cartModel.eventureListId = $routeParams.listId;
+    cartModel.participantId = $location.search()["uid"];
 
-        $scope.addPlayer = function() {
-            $scope.players.push({ name: "", email: "", position: "" });
-        };
-		
-		var promises = [];
-		common.activateController(promises, controllerId);
+    $scope.team = {};
+    $scope.team.teamName = "";
+    $scope.players = [{
+      name: "",
+      email: "",
+      position: ""
+    }];
 
-        $scope.makeTeam = function() {
-            var valid = true;
-            cartModel.teamName = $scope.team.teamName || "";
-            if (cartModel.teamName.length < minNameLength) {
-                // make name red
-                valid = false;
-            }
+    $scope.addPlayer = function () {
+      $scope.players.push({
+        name: "",
+        email: "",
+        position: ""
+      });
+    };
 
-            cartModel.teamMembers = [];
-            for (var i = 0; i < $scope.players.length; i++) {
-                var name, email;
-                if ($scope.players[i].name.length > minNameLength && $scope.players[i].email) {
-                    cartModel.teamMembers.push({ name: $scope.players[i].name, email: $scope.players[i].email, position: $scope.players[i].position });
-                } else if ($scope.players[i].name.length == 0 && $scope.players[i].email.length == 0) {
-                    // ignore this entry
-                    // it's still valid
-                } else {
-                    // make line red
-                    valid = false;
-                }
-            }
+    var promises = [];
+    common.activateController(promises, controllerId);
 
-            if (cartModel.teamMembers.length < minTeamSize) {
-                // do something
-                valid = false;
-            }
+    $scope.makeTeam = function () {
+      var valid = true;
 
-            if (valid) {
-				$location.search("uid", null);
-                $location.path("/eventure/"+ cartModel.eventureId + "/list/" + cartModel.eventureListId + "/team/" + cartModel.teamId + "/payment");
-            }
-        };
-    }
+      cartModel.division = $scope.team.division;
+      cartModel.timeFinish = $scope.team.timeFinish;
+      cartModel.teamName = $scope.team.teamName || "";
+      if (cartModel.teamName.length < minNameLength) {
+        // make name red
+        valid = false;
+      }
 
-    angular.module("evReg").controller(controllerId,
-        ["$scope", "$location", "$routeParams", "RegistrationCartModel", "common", controller]);
-	
+      cartModel.teamMembers = [];
+      for (var i = 0; i < $scope.players.length; i++) {
+        var name, email;
+        if ($scope.players[i].name.length > minNameLength && $scope.players[i].email) {
+          cartModel.teamMembers.push({
+            name: $scope.players[i].name,
+            email: $scope.players[i].email,
+            position: $scope.players[i].position
+          });
+        } else if ($scope.players[i].name.length == 0 && $scope.players[i].email.length == 0) {
+          // ignore this entry
+          // it's still valid
+        } else {
+          // make line red
+          valid = false;
+        }
+      }
+
+      if (cartModel.teamMembers.length < minTeamSize) {
+        // do something
+        valid = false;
+      }
+
+      if (valid) {
+        $location.search("uid", null);
+        $location.path("/eventure/" + cartModel.eventureId + "/list/" + cartModel.eventureListId + "/team/" + cartModel.teamId + "/payment");
+      }
+    };
+  }
+
+  angular.module("evReg").controller(controllerId, ["$scope", "$location", "$routeParams", "RegistrationCartModel", "common", controller]);
+
 })();
