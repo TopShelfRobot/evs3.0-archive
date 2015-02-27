48673b6fbaba67be51b3c13a553145feb0572823
Wil Goolsby <wgoolsby@me.com>
2015-02-27 10:43:18 -0500

    Change to team member receipt

--- app/common/services/repository.team.js
+++ app/common/services/repository.team.js
@@ -1,167 +1,190 @@
-﻿(function () {
-    'use strict';
+(function () {
+  'use strict';
 
-    var serviceId = 'repository.team';
+  var serviceId = 'repository.team';
 
-    angular.module('common').factory(serviceId,
-        ['breeze', 'repository.abstract', repositoryTeam]);
+  angular.module('common').factory(serviceId, ['breeze', 'repository.abstract', repositoryTeam]);
 
-    function repositoryTeam(breeze, abstractRepository) {
-        var entityName = 'team';
-        //var entityNames = model.entityNames;
-        var entityQuery = breeze.EntityQuery;
-        var predicate = breeze.Predicate;
+  function repositoryTeam(breeze, abstractRepository) {
+    var entityName = 'team';
+    //var entityNames = model.entityNames;
+    var entityQuery = breeze.EntityQuery;
+    var predicate = breeze.Predicate;
 
-        //only creating on demand - using ctor
-        function Ctor(mgr) {
-            this.serviceId = serviceId;
-            this.entityName = entityName;
-            this.manager = mgr;
-            // Exposed data access functions
-            this.getAll = getAll;
-            this.getTeamMemberPaymentInfoByTeamMemberGuid = getTeamMemberPaymentInfoByTeamMemberGuid;
-			this.getTeamById  = getTeamById;
-            this.getTeamMemberById = getTeamMemberById;
-			this.getTeamMembersByTeamId = getTeamMembersByTeamId;
-			this.addTeamMember = addTeamMember;
-			this.getTeamPaymentsByTeamId = getTeamPaymentsByTeamId;
-            this.getNotPaidTeamMemberCountByTeamGuid = getNotPaidTeamMemberCountByTeamGuid;
-            this.getTeamMemberPaymentSumByTeamGuid = getTeamMemberPaymentSumByTeamGuid;
-            this.GetTeamInfoByRegistrationId = GetTeamInfoByRegistrationId;
-        }
-
-        // Allow this repo to have access to the Abstract Repo's functions,
-        // then put its own Ctor back on itself.
-        Ctor.prototype = new abstractRepository(Ctor);
-        Ctor.prototype.constructor = Ctor;
-        abstractRepository.extend(Ctor);
-
-        return Ctor;
-
-        function getAll() {
-            var self = this;
-            return entityQuery.from('ZZZ')
-                .using(self.manager).execute()
-                .then(querySucceeded, self._queryFailed);
-
-            function querySucceeded(data) {
-                return data.results;
-            }
-        }
-        
-        function getTeamMemberPaymentInfoByTeamMemberGuid(guid) {
-            var self = this;
-            var query = entityQuery.from('getTeamMemberPaymentInfoByTeamMemberGuid')
-                .withParameters({ id: guid });
-
-            return self.manager.executeQuery(query)
-                .then(querySucceeded, self._queryFailed);
-
-            function querySucceeded(data) {
-                return data.results[0];
-            }
-        };
-
-        function GetTeamInfoByRegistrationId(id) {
-            var self = this;
-            var query = entityQuery.from('GetTeamInfoByRegistrationId')
-                .withParameters({ id: id });
-
-            return self.manager.executeQuery(query)
-                .then(querySucceeded, self._queryFailed);
-
-            function querySucceeded(data) {
-                return data.results[0];
-            }
-        };
-		
-		function getTeamById(id){
-			
-			var self = this;
-			
-			var query = entityQuery.from("Teams")
-				.where('id', '==', id);
-         
-		    return self.manager.executeQuery(query)
-                .then(querySucceeded, self.queryFailed);
-				
-            function querySucceeded(data) {
-                return data.results[0];
-            }
-        }
-
-        function getTeamMemberById(id){
-
-            var self = this;
-
-            var query = entityQuery.from("TeamMembers")
-                .where('id', '==', id);
-
-            return self.manager.executeQuery(query)
-                .then(querySucceeded, self.queryFailed);
-
-            function querySucceeded(data) {
-                return data.results[0];
-            }
-        }
-
-        function getNotPaidTeamMemberCountByTeamGuid(teamGuid) {
-            var self = this;
-            var query = entityQuery.from('GetNotPaidTeamMemberCountByTeamGuid')
-                .withParameters({ id: teamGuid });
-
-            return self.manager.executeQuery(query)
-                .then(querySucceeded, self._queryFailed);
-
-            function querySucceeded(data) {
-                return data.results[0];
-            }
-		}
-		
-		function getTeamMembersByTeamId(id){
-			var self = this;
-			
-			var query = entityQuery.from("Teammembers")
-				.where("teamId", "==", Number(id));
-				
-			return self.manager.executeQuery(query)
-				.then(querySucceeded, self.queryFailed);
-			
-            function querySucceeded(data) {
-                return data.results;
-            }	
-		}
-		
-		function addTeamMember(obj){
-            var self = this;
-            return self.manager.createEntity('TeamMember', obj);
-		}
-		
-		function getTeamPaymentsByTeamId(id){
-			var self = this;
-			
-			var query = entityQuery.from("TeamMemberPayments")
-				// .where("teamId", "==", Number(id));
-				
-			return self.manager.executeQuery(query)
-				.then(querySucceeded, self.queryFailed);
-			
-            function querySucceeded(data) {
-                return data.results;
-            }	
-		}
-        
-        function getTeamMemberPaymentSumByTeamGuid(teamGuid) {
-            var self = this;
-            var query = entityQuery.from('GetTeamMemberPaymentSumByTeamGuid')
-                .withParameters({ id: teamGuid });
-
-            return self.manager.executeQuery(query)
-                .then(querySucceeded, self._queryFailed);
-
-            function querySucceeded(data) {
-                return data.results[0];
-            }
-        }
+    //only creating on demand - using ctor
+    function Ctor(mgr) {
+      this.serviceId = serviceId;
+      this.entityName = entityName;
+      this.manager = mgr;
+      // Exposed data access functions
+      this.getAll = getAll;
+      this.getTeamMemberPaymentInfoByTeamMemberGuid = getTeamMemberPaymentInfoByTeamMemberGuid;
+      this.getTeamById = getTeamById;
+      this.getTeamByGuid = getTeamByGuid;
+      this.getTeamMemberById = getTeamMemberById;
+      this.getTeamMembersByTeamId = getTeamMembersByTeamId;
+      this.addTeamMember = addTeamMember;
+      this.getTeamPaymentsByTeamId = getTeamPaymentsByTeamId;
+      this.getNotPaidTeamMemberCountByTeamGuid = getNotPaidTeamMemberCountByTeamGuid;
+      this.getTeamMemberPaymentSumByTeamGuid = getTeamMemberPaymentSumByTeamGuid;
+      this.GetTeamInfoByRegistrationId = GetTeamInfoByRegistrationId;
     }
+
+    // Allow this repo to have access to the Abstract Repo's functions,
+    // then put its own Ctor back on itself.
+    Ctor.prototype = new abstractRepository(Ctor);
+    Ctor.prototype.constructor = Ctor;
+    abstractRepository.extend(Ctor);
+
+    return Ctor;
+
+    function getAll() {
+      var self = this;
+      return entityQuery.from('ZZZ')
+        .using(self.manager).execute()
+        .then(querySucceeded, self._queryFailed);
+
+      function querySucceeded(data) {
+        return data.results;
+      }
+    }
+
+    function getTeamMemberPaymentInfoByTeamMemberGuid(guid) {
+      var self = this;
+      var query = entityQuery.from('getTeamMemberPaymentInfoByTeamMemberGuid')
+        .withParameters({
+          id: guid
+        });
+
+      return self.manager.executeQuery(query)
+        .then(querySucceeded, self._queryFailed);
+
+      function querySucceeded(data) {
+        return data.results[0];
+      }
+    };
+
+    function GetTeamInfoByRegistrationId(id) {
+      var self = this;
+      var query = entityQuery.from('GetTeamInfoByRegistrationId')
+        .withParameters({
+          id: id
+        });
+
+      return self.manager.executeQuery(query)
+        .then(querySucceeded, self._queryFailed);
+
+      function querySucceeded(data) {
+        return data.results[0];
+      }
+    };
+
+    function getTeamById(id) {
+
+      var self = this;
+
+      var query = entityQuery.from("Teams")
+        .where('id', '==', id);
+
+      return self.manager.executeQuery(query)
+        .then(querySucceeded, self.queryFailed);
+
+      function querySucceeded(data) {
+        return data.results[0];
+      }
+    }
+
+    function getTeamByGuid(guid) {
+
+      var self = this;
+
+      var query = entityQuery.from("Teams")
+        .where('guid', '==', guid);
+
+      return self.manager.executeQuery(query)
+        .then(querySucceeded, self.queryFailed);
+
+      function querySucceeded(data) {
+        return data.results[0];
+      }
+    }
+
+    function getTeamMemberById(id) {
+
+      var self = this;
+
+      var query = entityQuery.from("TeamMembers")
+        .where('id', '==', id);
+
+      return self.manager.executeQuery(query)
+        .then(querySucceeded, self.queryFailed);
+
+      function querySucceeded(data) {
+        return data.results[0];
+      }
+    }
+
+    function getNotPaidTeamMemberCountByTeamGuid(teamGuid) {
+      var self = this;
+      var query = entityQuery.from('GetNotPaidTeamMemberCountByTeamGuid')
+        .withParameters({
+          id: teamGuid
+        });
+
+      return self.manager.executeQuery(query)
+        .then(querySucceeded, self._queryFailed);
+
+      function querySucceeded(data) {
+        return data.results[0];
+      }
+    }
+
+    function getTeamMembersByTeamId(id) {
+      var self = this;
+
+      var query = entityQuery.from("Teammembers")
+        .where("teamId", "==", Number(id));
+
+      return self.manager.executeQuery(query)
+        .then(querySucceeded, self.queryFailed);
+
+      function querySucceeded(data) {
+        return data.results;
+      }
+    }
+
+    function addTeamMember(obj) {
+      var self = this;
+      return self.manager.createEntity('TeamMember', obj);
+    }
+
+    function getTeamPaymentsByTeamId(id) {
+      var self = this;
+
+      var query = entityQuery.from("TeamMemberPayments")
+        // .where("teamId", "==", Number(id));
+
+      return self.manager.executeQuery(query)
+        .then(querySucceeded, self.queryFailed);
+
+      function querySucceeded(data) {
+        return data.results;
+      }
+    }
+
+    function getTeamMemberPaymentSumByTeamGuid(teamGuid) {
+      var self = this;
+      var query = entityQuery.from('GetTeamMemberPaymentSumByTeamGuid')
+        .withParameters({
+          id: teamGuid
+        });
+
+      return self.manager.executeQuery(query)
+        .then(querySucceeded, self._queryFailed);
+
+      function querySucceeded(data) {
+        return data.results[0];
+      }
+    }
+  }
 })();
--- app/registration/config.route.js
+++ app/registration/config.route.js
@@ -1,210 +1,212 @@
-﻿(function () {
-	'use strict';
+(function () {
+  'use strict';
 
-	var app = angular.module('evReg');
+  var app = angular.module('evReg');
 
-	// Collect the routes
-	app.constant('reg.routes', getRoutes());
+  // Collect the routes
+  app.constant('reg.routes', getRoutes());
 
-	// Configure the routes and route resolvers
-	app.config(['$routeProvider', 'reg.routes', routeConfigurator]);
-	
-	function routeConfigurator($routeProvider, routes) {
-		routes.forEach(function (r) {
-			// $routeProvider.when(r.url, r.config);
-			setRoute(r.url, r.config);
-		});
-		$routeProvider.otherwise({ redirectTo: '/eventure' });
+  // Configure the routes and route resolvers
+  app.config(['$routeProvider', 'reg.routes', routeConfigurator]);
+
+  function routeConfigurator($routeProvider, routes) {
+    routes.forEach(function (r) {
+      // $routeProvider.when(r.url, r.config);
+      setRoute(r.url, r.config);
+    });
+    $routeProvider.otherwise({
+      redirectTo: '/eventure'
+    });
 
 
-		function setRoute(url, definition) {
-			// Sets resolvers for all of the routes
-			// by extending any existing resolvers (or creating a new one).
-			definition.resolve = angular.extend(definition.resolve || {}, {
-				prime: prime
-			});
-			$routeProvider.when(url, definition);
-			return $routeProvider;
-		}
-	}
+    function setRoute(url, definition) {
+      // Sets resolvers for all of the routes
+      // by extending any existing resolvers (or creating a new one).
+      definition.resolve = angular.extend(definition.resolve || {}, {
+        prime: prime
+      });
+      $routeProvider.when(url, definition);
+      return $routeProvider;
+    }
+  }
 
-	prime.$inject = ['datacontext'];
-	
-	function prime(dc) { 
-		return dc.prime(); 
-	}
+  prime.$inject = ['datacontext'];
 
-	// Define the routes
-	function getRoutes() {
-		return [
-			{
-				url: '/eventure',
-				config: {
-					title: 'Eventure',
-					templateUrl: 'app/registration/standard/eventure.part.html',
-					settings: {
-						nav: 2,
-						content: 'Events'
-					}
-				}
-			}, {
-				url: '/eventure/:eventureId/list/',
-				config: {
-					title: 'Eventure List',
-					templateUrl: 'app/registration/standard/eventurelist.part.html',
-					settings: {
-						nav: 3,
-						content: 'Participants'
-					}
-				}
-			}, {
-				url: '/eventure/:eventureId/list/:listId/team',
-				config: {
-					title: 'Create Team',
-					templateUrl: 'app/registration/team/createteam.part.html',
-					settings: {
-						nav: 1,
-						content: 'Enterprise'
-					}
-				}
-			}, {
-				url: '/eventure/:eventureId/list/:listId/team/:teamId/payment',
-				config: {
-					title: 'Team Payment',
-					templateUrl: 'app/registration/team/teamPayment.part.html',
-					settings: {
-						nav: 4,
-						content: 'Coupons & Addons'
-					}
-				}
-			}, {
-				url: '/team/:teamGuid/member/:teamMemberGuid/payment',
-				config: {
-					title: 'Member Payment',
-					templateUrl: 'app/registration/team/memberPayment.part.html'
-				}
-			}, {
-				url: '/participant',
-				config: {
-					title: 'Participant',
-					templateUrl: 'app/registration/profile/setparticipant.part.html'
-				}
-			}, {
-				url: '/participant/add',
-				config: {
-					title: 'Add Participant',
-					templateUrl: 'app/registration/profile/addparticipant.part.html'
-				}
-			}, {
-				url: '/participant/:partId',
-				config: {
-					title: 'Edit Participant',
-					templateUrl: 'app/registration/profile/setparticipant.part.html'
-				}
-			}, {
-				url: '/eventure/:eventureId/list/:listId/questions',
-				config: {
-					title: 'Questions',
-					templateUrl: 'app/registration/standard/questions.part.html'
-				}
-			}, {
-				url: '/confirm',
-				config: {
-					title: 'Confirm',
-					templateUrl: 'app/registration/payment/confirm.part.html'
-				}
-			}, {
-				url: '/receipt/:registrationId',
-				config: {
-					title: 'Receipt',
-					templateUrl: 'app/registration/payment/teamMemberReceipt.part.html'
-				}
-			}, {
-				url: '/orderreceipt/:orderId',
-				config: {
-					title: 'Receipt Cart',
-					templateUrl: 'app/registration/payment/orderreceipt.part.html'
-				}
-			}, {
-				url: '/new-user/add',
-				config: {
-					title: 'Add User Profile',
-					templateUrl: 'app/registration/profile/addProfile.part.html'
-				}
-			}, {
-				url: '/user-profile/:participantId',
-				config: {
-					title: 'User Profile',
-					templateUrl: 'app/registration/profile/profile.part.html'
-				}
-			},{
-				url: '/editteam/:teamId',
-				config: {
-					title: 'Edit Team',
-					templateUrl: 'app/registration/team/editteam.part.html'
-				}
-			}, {
-				url: '/registration/:regId',
-				config: {
-					title: 'Edit Registration',
-					templateUrl: 'app/registration/profile/registrationedit.part.html'
-				}
-			}, {
-				url: '/registration/:regId/edit',
-				config: {
-					title: 'Edit Registration',
-					templateUrl: 'app/registration/profile/registrationEditConfirm.part.html'
-				}
-			}, {
-				url: '/registration/:regId/transferQuestions',
-				config: {
-					title: 'Edit Registration',
-					templateUrl: 'app/registration/profile/transferQuestions.part.html'
-				}
-			}, {
-				url: '/shoppingcart',
-				config: {
-					title: 'Shopping Cart',
-					templateUrl: 'app/registration/_layout/shoppingcart.part.html'
-				}
-			}, {
-				url: '/login',
-				config: {
-					title: 'Login',
-					templateUrl: 'app/registration/authentication/login.part.html'
-				}
-			}, {
-				url: '/login/:requestPath*',
-				config: {
-					title: 'Login',
-					templateUrl: 'app/registration/authentication/login.part.html'
-				}
-			}, {
-				url: '/signup',
-				config: {
-					title: 'Sign-Up',
-					templateUrl: 'app/registration/authentication/signup.part.html'
-				}
-			}, {
-				url: '/forgotpassword',
-				config: {
-					title: 'Forgot Password',
-					templateUrl: 'app/registration/authentication/forgotpassword.part.html'
-				}
-			}, {
-				url: '/resetpassword',
-				config: {
-					title: 'Reset Password',
-					templateUrl: 'app/registration/authentication/resetpassword.part.html'
-				}
-			}
-			//, {
-			//    url: '/resetpassword/:token',
-			//    config: {
-			//        title: 'Reset Password',
-			//        templateUrl: 'app/registration/views/resetpassword.part.html'
-			//    }
-			//}
-		];
-	}
+  function prime(dc) {
+    return dc.prime();
+  }
+
+  // Define the routes
+  function getRoutes() {
+    return [
+      {
+        url: '/eventure',
+        config: {
+          title: 'Eventure',
+          templateUrl: 'app/registration/standard/eventure.part.html',
+          settings: {
+            nav: 2,
+            content: 'Events'
+          }
+        }
+   }, {
+        url: '/eventure/:eventureId/list/',
+        config: {
+          title: 'Eventure List',
+          templateUrl: 'app/registration/standard/eventurelist.part.html',
+          settings: {
+            nav: 3,
+            content: 'Participants'
+          }
+        }
+   }, {
+        url: '/eventure/:eventureId/list/:listId/team',
+        config: {
+          title: 'Create Team',
+          templateUrl: 'app/registration/team/createteam.part.html',
+          settings: {
+            nav: 1,
+            content: 'Enterprise'
+          }
+        }
+   }, {
+        url: '/eventure/:eventureId/list/:listId/team/:teamId/payment',
+        config: {
+          title: 'Team Payment',
+          templateUrl: 'app/registration/team/teamPayment.part.html',
+          settings: {
+            nav: 4,
+            content: 'Coupons & Addons'
+          }
+        }
+   }, {
+        url: '/team/:teamGuid/member/:teamMemberGuid/payment',
+        config: {
+          title: 'Member Payment',
+          templateUrl: 'app/registration/team/memberPayment.part.html'
+        }
+   }, {
+        url: '/participant',
+        config: {
+          title: 'Participant',
+          templateUrl: 'app/registration/profile/setparticipant.part.html'
+        }
+   }, {
+        url: '/participant/add',
+        config: {
+          title: 'Add Participant',
+          templateUrl: 'app/registration/profile/addparticipant.part.html'
+        }
+   }, {
+        url: '/participant/:partId',
+        config: {
+          title: 'Edit Participant',
+          templateUrl: 'app/registration/profile/setparticipant.part.html'
+        }
+   }, {
+        url: '/eventure/:eventureId/list/:listId/questions',
+        config: {
+          title: 'Questions',
+          templateUrl: 'app/registration/standard/questions.part.html'
+        }
+   }, {
+        url: '/confirm',
+        config: {
+          title: 'Confirm',
+          templateUrl: 'app/registration/payment/confirm.part.html'
+        }
+   }, {
+        url: '/member-receipt/:teamGuid',
+        config: {
+          title: 'Receipt',
+          templateUrl: 'app/registration/payment/teamMemberReceipt.part.html'
+        }
+   }, {
+        url: '/orderreceipt/:orderId',
+        config: {
+          title: 'Receipt Cart',
+          templateUrl: 'app/registration/payment/orderreceipt.part.html'
+        }
+   }, {
+        url: '/new-user/add',
+        config: {
+          title: 'Add User Profile',
+          templateUrl: 'app/registration/profile/addProfile.part.html'
+        }
+   }, {
+        url: '/user-profile/:participantId',
+        config: {
+          title: 'User Profile',
+          templateUrl: 'app/registration/profile/profile.part.html'
+        }
+   }, {
+        url: '/editteam/:teamId',
+        config: {
+          title: 'Edit Team',
+          templateUrl: 'app/registration/team/editteam.part.html'
+        }
+   }, {
+        url: '/registration/:regId',
+        config: {
+          title: 'Edit Registration',
+          templateUrl: 'app/registration/profile/registrationedit.part.html'
+        }
+   }, {
+        url: '/registration/:regId/edit',
+        config: {
+          title: 'Edit Registration',
+          templateUrl: 'app/registration/profile/registrationEditConfirm.part.html'
+        }
+   }, {
+        url: '/registration/:regId/transferQuestions',
+        config: {
+          title: 'Edit Registration',
+          templateUrl: 'app/registration/profile/transferQuestions.part.html'
+        }
+   }, {
+        url: '/shoppingcart',
+        config: {
+          title: 'Shopping Cart',
+          templateUrl: 'app/registration/_layout/shoppingcart.part.html'
+        }
+   }, {
+        url: '/login',
+        config: {
+          title: 'Login',
+          templateUrl: 'app/registration/authentication/login.part.html'
+        }
+   }, {
+        url: '/login/:requestPath*',
+        config: {
+          title: 'Login',
+          templateUrl: 'app/registration/authentication/login.part.html'
+        }
+   }, {
+        url: '/signup',
+        config: {
+          title: 'Sign-Up',
+          templateUrl: 'app/registration/authentication/signup.part.html'
+        }
+   }, {
+        url: '/forgotpassword',
+        config: {
+          title: 'Forgot Password',
+          templateUrl: 'app/registration/authentication/forgotpassword.part.html'
+        }
+   }, {
+        url: '/resetpassword',
+        config: {
+          title: 'Reset Password',
+          templateUrl: 'app/registration/authentication/resetpassword.part.html'
+        }
+   }
+   //, {
+   //    url: '/resetpassword/:token',
+   //    config: {
+   //        title: 'Reset Password',
+   //        templateUrl: 'app/registration/views/resetpassword.part.html'
+   //    }
+   //}
+  ];
+  }
 })();
