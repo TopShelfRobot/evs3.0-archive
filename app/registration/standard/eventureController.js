9a1573955352405980a0c402fc21aa3229b3f5e6
Wil Goolsby <wgoolsby@me.com>
2015-03-11 15:30:41 -0400

    Fix pagination issue on eventure

--- app/registration/standard/eventureController.js
+++ app/registration/standard/eventureController.js
@@ -1,70 +1,71 @@
 (function () {
 
-	var controllerId = "EventureController";
+  var controllerId = "EventureController";
 
-	function controller($scope, $location, $anchorScroll, config, datacontext, cart, common) {
+  function controller($scope, $location, $anchorScroll, config, datacontext, cart, common) {
 
-		//$scope.cart = cartModel;
+    //$scope.cart = cartModel;
 
-		var all = [];
-		var viewLength = 12;
-		var currentPage = 0;
-		$scope.cart = cart;
-		//alert('heryhr');
-	    //datacontext.getEventuresByOwnerId(config.owner.ownerId)
+    var all = [];
+    var viewLength = 12;
+    var currentPage = 0;
+    $scope.cart = cart;
+    //alert('heryhr');
+    //datacontext.getEventuresByOwnerId(config.owner.ownerId)
 
-		$scope.isPaginationVisible = false;
+    $scope.isPaginationVisible = false;
 
-		$scope.eventureName = cart.regSettings.eventureName;
-		$scope.registerButtonText = cart.regSettings.registerButtonText;
+    $scope.eventureName = cart.regSettings.eventureName;
+    $scope.registerButtonText = cart.regSettings.registerButtonText;
 
-		//console.log(cart.ownerId);
-		//console.log(cart.regSettings.eventureName);
+    //console.log(cart.ownerId);
+    //console.log(cart.regSettings.eventureName);
 
-		var promises = [];
-		promises.push(
-			datacontext.eventure.getEventuresByOwnerId(cart.ownerId)
-				.then(function (list) {
-					all = list;
-					$scope.eventures = all.slice(0, viewLength);
-				})
-		);
+    var promises = [];
+    promises.push(
+      datacontext.eventure.getEventuresByOwnerId(cart.ownerId)
+      .then(function (list) {
+        all = list;
+        if (all.length > viewLength) {
+          $scope.isPaginationVisible = true;
+        }
+        $scope.eventures = all.slice(0, viewLength);
+      })
+    );
 
-		if(all.length > viewLength) {
-			$scope.isPaginationVisible = true;
-		}
 
-		common.activateController(promises, controllerId);
 
-		$scope.NavigateToList = function (listId) {
-		    //#/eventure/{{eventure.id}}/list
-		    //alert(listId);
-		    listpath = '/eventure/' + listId + '/list';
-		    cart.navUrl = listpath;
-		    $location.path(listpath);
-		};
+    common.activateController(promises, controllerId);
 
-		$scope.nextPage = function () {
-			if (all.length >= (currentPage + 1) * viewLength) {
-				currentPage++;
-				$scope.eventures = all.slice(currentPage * viewLength, (currentPage + 1) * viewLength);
-				$location.hash("top");
-				$anchorScroll();
-				$location.hash("");
-			}
-		};
+    $scope.NavigateToList = function (listId) {
+      //#/eventure/{{eventure.id}}/list
+      //alert(listId);
+      listpath = '/eventure/' + listId + '/list';
+      cart.navUrl = listpath;
+      $location.path(listpath);
+    };
 
-		$scope.prevPage = function () {
-			if (currentPage !== 0) {
-				currentPage--;
-				$scope.eventures = all.slice(currentPage * viewLength, (currentPage + 1) * viewLength);
-				$location.hash("top");
-				$anchorScroll();
-				$location.hash("");
-			}
-		};
-	}
+    $scope.nextPage = function () {
+      if (all.length >= (currentPage + 1) * viewLength) {
+        currentPage++;
+        $scope.eventures = all.slice(currentPage * viewLength, (currentPage + 1) * viewLength);
+        $location.hash("top");
+        $anchorScroll();
+        $location.hash("");
+      }
+    };
 
-	angular.module("evReg").controller(controllerId, ["$scope", "$location", "$anchorScroll", "config", "datacontext", "CartModel", "common", controller]);
+    $scope.prevPage = function () {
+      if (currentPage !== 0) {
+        currentPage--;
+        $scope.eventures = all.slice(currentPage * viewLength, (currentPage + 1) * viewLength);
+        $location.hash("top");
+        $anchorScroll();
+        $location.hash("");
+      }
+    };
+  }
+
+  angular.module("evReg").controller(controllerId, ["$scope", "$location", "$anchorScroll", "config", "datacontext", "CartModel", "common", controller]);
 
 })();
