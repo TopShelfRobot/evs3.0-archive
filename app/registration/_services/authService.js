21f8f9b66dddc36a88eccfea2b0174164d4e50b3
Bill Burke <billstron@gmail.com>
2015-03-13 05:48:02 -0400

    Added missing dependency ()

--- app/registration/_services/authService.js
+++ app/registration/_services/authService.js
@@ -1,7 +1,7 @@
 //var testRoles = ['user', 'admin', 'super-user', 'money'];
 
-angular.module('evReg').factory('authService', ['$http', '$q', 'UserAgent', 'localStorageService', 'ngAuthSettings', 'CartModel', 'config',
-    function ($http, $q, userAgent, localStorageService, ngAuthSettings, cart, config) {
+angular.module('evReg').factory('authService', ['$http', '$q', "$timeout", 'UserAgent', 'localStorageService', 'ngAuthSettings', 'CartModel', 'config',
+    function ($http, $q, $timeout, userAgent, localStorageService, ngAuthSettings, cart, config) {
 
         'use strict';
 
