a7ffb33ed6b2a3a78231f637e8e8e6285dd68e2f
Wil Goolsby <wgoolsby@me.com>
2015-03-17 15:33:04 -0400

    revert to original jshint options

--- app/registration/authentication/signupController.js
+++ app/registration/authentication/signupController.js
@@ -1,50 +1,49 @@
-;
 (function () {
-    'use strict';
+	'use strict';
 
-    var controllerId = "signupController";
+	var controllerId = "signupController";
 
-    function Controller($scope, $location, $timeout, authService, datacontext, cart, common) {
+	function Controller($scope, $location, $timeout, authService, datacontext, cart, common) {
 
-        $scope.savedSuccessfully = false;
-        $scope.message = "";
+		$scope.savedSuccessfully = false;
+		$scope.message = "";
 
-        $scope.registration = {
-            userName: "",
-            password: "",
-            confirmPassword: ""
-        };
+		$scope.registration = {
+			userName: "",
+			password: "",
+			confirmPassword: ""
+		};
 
-        $scope.isDash = false;
+		$scope.isDash = false;
 
-        var requestPath = window.location.pathname;
+		var requestPath = window.location.pathname;
 
-        if (requestPath === '/dash.html') {
-            $scope.isDash = true;
-        }
+		if (requestPath === '/dash.html') {
+			$scope.isDash = true;
+		}
 
-        common.activateController(controllerId);
+		common.activateController(controllerId);
 
-        $scope.signUp = function () {
+		$scope.signUp = function () {
 
-            //alert('yo yoy yo');
-            authService.saveRegistration($scope.registration, true).then(function (response) {
+			//alert('yo yoy yo');
+			authService.saveRegistration($scope.registration, true).then(function (response) {
 
-                $scope.savedSuccessfully = true;
-                $scope.message = "User has been registered successfully, you will be redirected in 2 seconds.";
-                $scope.message = authService.startTimer($scope.registration);
-            },
-              function (response) {
-                  var errors = [];
-                  for (var key in response.data.modelState) {
-                      for (var i = 0; i < response.data.modelState[key].length; i++) {
-                          errors.push(response.data.modelState[key][i]);
-                      }
-                  }
-                  $scope.message = "Failed to register user due to:" + errors.join(' ');
-              });
-        };
-    }
+					$scope.savedSuccessfully = true;
+					$scope.message = "User has been registered successfully, you will be redirected in 2 seconds.";
+					$scope.message = authService.startTimer($scope.registration);
+				},
+				function (response) {
+					var errors = [];
+					for (var key in response.data.modelState) {
+						for (var i = 0; i < response.data.modelState[key].length; i++) {
+							errors.push(response.data.modelState[key][i]);
+						}
+					}
+					$scope.message = "Failed to register user due to:" + errors.join(' ');
+				});
+		};
+	}
 
-    angular.module("evReg").controller(controllerId, ['$scope', '$location', '$timeout', 'authService', 'datacontext', 'CartModel', 'common', Controller]);
+	angular.module("evReg").controller(controllerId, ['$scope', '$location', '$timeout', 'authService', 'datacontext', 'CartModel', 'common', Controller]);
 })();
--- gruntfile.js
+++ gruntfile.js
@@ -94,69 +94,13 @@ module.exports = function (grunt) {
 		},
 		jshint: {
 			options: {
-				'bitwise': true,
-				'camelcase': true,
-				'curly': true,
-				'eqeqeq': true,
-				'es3': false,
-				'forin': true,
-				'freeze': true,
-				'immed': true,
-				'indent': 4,
-				'latedef': 'nofunc',
-				'newcap': false,
-				'noarg': true,
-				'noempty': true,
-				'nonbsp': true,
-				'nonew': true,
-				'plusplus': false,
-				'quotmark': 'single',
-				'undef': true,
-				'unused': false,
-				'strict': false,
-				'maxparams': 10,
-				'maxdepth': 5,
-				'maxstatements': 40,
-				'maxcomplexity': 8,
-				'maxlen': 420,
-
-				'asi': false,
-				'boss': false,
-				'debug': false,
-				'eqnull': true,
-				'esnext': false,
-				'evil': false,
-				'expr': false,
-				'funcscope': false,
-				'globalstrict': false,
-				'iterator': false,
-				'lastsemic': false,
-				'laxbreak': false,
-				'laxcomma': false,
-				'loopfunc': true,
-				'maxerr': false,
-				'moz': false,
-				'multistr': false,
-				'notypeof': false,
-				'proto': false,
-				'scripturl': false,
-				'shadow': false,
-				'sub': true,
-				'supernew': false,
-				'validthis': true,
-				'noyield': false,
-
-				'browser': true,
-				'devel': true,
-				'node': true,
-
-				'globals': {
-					'angular': false,
-					'$': false,
-					'toastr': false,
-					'moment': false,
-					'kendo': false
-				}
+				multistr: true,
+				eqnull: true,
+				ignores: 'app/**/*.min.js',
+				force: true,
+				validthis: true,
+				expr: true,
+				newcap: false,
 			},
 			source: ['app/**/*.js']
 		},
@@ -191,5 +135,5 @@ module.exports = function (grunt) {
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	// Default grunt tasks
-	grunt.registerTask('default', ['sass', 'cssmin', 'concat', 'uglify']);
+	grunt.registerTask('default', ['jshint', 'sass', 'cssmin', 'concat', 'uglify']);
 };
