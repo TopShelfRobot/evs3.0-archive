97a2d95916478928248935c1bce1d55e32a71130
Wil Goolsby <wgoolsby@me.com>
2015-03-23 16:08:29 -0400

    Try a manual login flow

--- app/registration/app.js
+++ app/registration/app.js
@@ -39,7 +39,7 @@
 
 		window.fbAsyncInit = function () {
 			FB.init({
-			    appId: '611459955620419',   //1616809328551404',
+				appId: '1616809328551404', //611459955620419' //1616809328551404',
 				xfbml: true,
 				version: 'v2.2'
 			});
@@ -47,12 +47,10 @@
 
 		(function (d, s, id) {
 			var js, fjs = d.getElementsByTagName(s)[0];
-			if (d.getElementById(id)) {
-				return;
-			}
+			if (d.getElementById(id)) return;
 			js = d.createElement(s);
 			js.id = id;
-			js.src = '//connect.facebook.net/en_US/sdk.js';
+			js.src = "//connect.facebook.net/en_US/sdk.js";
 			fjs.parentNode.insertBefore(js, fjs);
 		}(document, 'script', 'facebook-jssdk'));
 
