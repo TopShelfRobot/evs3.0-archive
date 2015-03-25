d15ce687f8351dc12b7cc9e1bf6d48b921f53d84
Wil Goolsby <wgoolsby@me.com>
2015-03-25 10:47:01 -0400

    USAT

--- app/registration/standard/eventurelistController.js
+++ app/registration/standard/eventurelistController.js
@@ -1,12 +1,12 @@
 (function () {
 
 	var controllerId = "EventureListController";
-	angular.module("evReg").controller(controllerId,
-					["$scope", "$location", "$routeParams", "config", "CartModel", "datacontext", "common", "AgeService", controller]);
+	angular.module("evReg").controller(controllerId, ["$scope", "$location", "$routeParams", "config", "CartModel", "datacontext", "common", "AgeService", controller]);
 
 	function controller($scope, $location, $routeParams, config, cart, datacontext, common, dt) {
 
 		$scope.cart = cart;
+		$scope.usatMembershipId;
 
 		var promises = [];
 
@@ -18,30 +18,30 @@
 
 		promises.push(
 			datacontext.eventure.getEventureById($routeParams.eventureId)
-				.then(function (item) {
-					$scope.eventure = item;
-				})
+			.then(function (item) {
+				$scope.eventure = item;
+			})
 		);
 
 		promises.push(
 			datacontext.eventure.getEventureListsByEventureId($routeParams.eventureId)
-				.then(function (list) {
-					$scope.list = list;
-					$scope.selection = $scope.list[0];
-				})
+			.then(function (list) {
+				$scope.list = list;
+				$scope.selection = $scope.list[0];
+			})
 		);
 
 		//alert(cart.houseId);
 		//alert('cart.houseId');
 		promises.push(
-		   datacontext.participant.getParticipantsByHouseId(cart.houseId)
-					.then(function (list) {
-					    //console.log(list)
-						$scope.selectedParticipant = list[0];
-						$scope.participants = list;
-						$scope.age = dt.age($scope.selectedParticipant.dateBirth);
-						//console.log($scope.age);
-					})
+			datacontext.participant.getParticipantsByHouseId(cart.houseId)
+			.then(function (list) {
+				//console.log(list)
+				$scope.selectedParticipant = list[0];
+				$scope.participants = list;
+				$scope.age = dt.age($scope.selectedParticipant.dateBirth);
+				//console.log($scope.age);
+			})
 		);
 
 		common.activateController(promises, controllerId);
@@ -52,12 +52,11 @@
 			console.log(eventureList.eventureListType);
 
 
-			if(($scope.selection.minAge && $scope.age < $scope.selection.minAge) ||
+			if (($scope.selection.minAge && $scope.age < $scope.selection.minAge) ||
 				($scope.selection.maxAge && $scope.age > $scope.selection.maxAge)) {
 				alert("You do not meet the age restrictions for this event. You must be between " + $scope.selection.minAge + " and " + $scope.selection.maxAge);
-			}
-			else {
-				if (eventureList.eventureListType == "Standard") {   //enum? mjb
+			} else {
+				if (eventureList.eventureListType == "Standard") { //enum? mjb
 					$location.path("/eventure/" + eventure.id + "/list/" + eventureList.id + "/questions")
 						.search("uid", participant.id);
 				} else {
@@ -65,6 +64,23 @@
 						.search("uid", participant.id);
 				}
 			}
+
+			$scope.verifyUSAT = function () {
+				//alert('we in');
+				var source = {
+					'USATNumber': $scope.usatMembershipId
+				};
+				$http.post(config.apiPath + 'api/transaction/USATVerification', source)
+					.success(function (result) {
+						console.log('Success', result);
+					})
+					.error(function (data, status, headers, config) {
+						console.log('Error', data);
+					})
+					.finally(function () {
+
+					});
+			};
 
 			//if (eventureList.eventureListType == config.eventureListType.standard) {   //enum? mjb
 			//	$location.path("/eventure/" + eventure.id + "/list/" + eventureList.id + "/questions")
