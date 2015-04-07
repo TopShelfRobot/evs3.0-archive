487ca69596a5a52d421f4925c6ca5a55d82e341a
boone <boone.mike@gmail.com>
2015-04-07 13:44:44 -0400

    fadded send email to edit team

--- app/common/services/repository.team.js
+++ app/common/services/repository.team.js
@@ -1,194 +1,198 @@
 (function () {
-  'use strict';
+    'use strict';
 
-  var serviceId = 'repository.team';
+    var serviceId = 'repository.team';
 
-  angular.module('common').factory(serviceId, ['breeze', 'repository.abstract', repositoryTeam]);
+    angular.module('common').factory(serviceId, ['breeze', 'repository.abstract', repositoryTeam]);
 
-  function repositoryTeam(breeze, abstractRepository) {
-    var entityName = 'team';
-    //var entityNames = model.entityNames;
-    var entityQuery = breeze.EntityQuery;
-    var predicate = breeze.Predicate;
+    function repositoryTeam(breeze, abstractRepository) {
+        var entityName = 'team';
+        //var entityNames = model.entityNames;
+        var entityQuery = breeze.EntityQuery;
+        var predicate = breeze.Predicate;
 
-    //only creating on demand - using ctor
-    function Ctor(mgr) {
-      this.serviceId = serviceId;
-      this.entityName = entityName;
-      this.manager = mgr;
-      // Exposed data access functions
-      this.getAll = getAll;
-      this.getTeamMemberPaymentInfoByTeamMemberGuid = getTeamMemberPaymentInfoByTeamMemberGuid;
-      this.getTeamById = getTeamById;
-      this.getTeamByGuid = getTeamByGuid;
-      this.getTeamMemberById = getTeamMemberById;
-      this.getTeamMembersByTeamId = getTeamMembersByTeamId;
-      this.addTeamMember = addTeamMember;
-      this.getTeamPaymentsByTeamId = getTeamPaymentsByTeamId;
-      this.getNotPaidTeamMemberCountByTeamGuid = getNotPaidTeamMemberCountByTeamGuid;
-      this.getTeamMemberPaymentSumByTeamGuid = getTeamMemberPaymentSumByTeamGuid;
-      this.GetTeamInfoByRegistrationId = GetTeamInfoByRegistrationId;
+        //only creating on demand - using ctor
+        function Ctor(mgr) {
+            this.serviceId = serviceId;
+            this.entityName = entityName;
+            this.manager = mgr;
+            // Exposed data access functions
+            this.getAll = getAll;
+            this.getTeamMemberPaymentInfoByTeamMemberGuid = getTeamMemberPaymentInfoByTeamMemberGuid;
+            this.getTeamById = getTeamById;
+            this.getTeamByGuid = getTeamByGuid;
+            this.getTeamMemberById = getTeamMemberById;
+            this.getTeamMembersByTeamId = getTeamMembersByTeamId;
+            this.addTeamMember = addTeamMember;
+            this.getTeamPaymentsByTeamId = getTeamPaymentsByTeamId;
+            this.getNotPaidTeamMemberCountByTeamGuid = getNotPaidTeamMemberCountByTeamGuid;
+            this.getTeamMemberPaymentSumByTeamGuid = getTeamMemberPaymentSumByTeamGuid;
+            this.GetTeamInfoByRegistrationId = GetTeamInfoByRegistrationId;
+        }
+
+        // Allow this repo to have access to the Abstract Repo's functions,
+        // then put its own Ctor back on itself.
+        Ctor.prototype = new abstractRepository(Ctor);
+        Ctor.prototype.constructor = Ctor;
+        abstractRepository.extend(Ctor);
+
+        return Ctor;
+
+        function getAll() {
+            var self = this;
+            return entityQuery.from('ZZZ')
+              .using(self.manager).execute()
+              .then(querySucceeded, self._queryFailed);
+
+            function querySucceeded(data) {
+                return data.results;
+            }
+        }
+
+        function getTeamMemberPaymentInfoByTeamMemberGuid(guid) {
+            var self = this;
+            var query = entityQuery.from('getTeamMemberPaymentInfoByTeamMemberGuid')
+              .withParameters({
+                  id: guid
+              });
+
+            return self.manager.executeQuery(query)
+              .then(querySucceeded, self._queryFailed);
+
+            function querySucceeded(data) {
+                return data.results[0];
+            }
+        };
+
+        function GetTeamInfoByRegistrationId(id) {
+            var self = this;
+            var query = entityQuery.from('GetTeamInfoByRegistrationId')
+              .withParameters({
+                  id: id
+              });
+
+            return self.manager.executeQuery(query)
+                .then(querySucceeded, self._queryFailed);
+
+            function querySucceeded(data) {
+                return data.results[0];
+            }
+        };
+
+        function getTeamById(id) {
+
+            var self = this;
+
+            var query = entityQuery.from("Teams")
+              .where('id', '==', id);
+
+            return self.manager.executeQuery(query)
+              .then(querySucceeded, self.queryFailed);
+
+            function querySucceeded(data) {
+                return data.results[0];
+            }
+        }
+
+        function getTeamByGuid(guid) {
+
+            console.log('in the dccccc');
+
+            var self = this;
+
+            var query = entityQuery.from("Teams")
+              .where('teamGuid', '==', guid);
+
+            console.log('calling quesrty');
+            return self.manager.executeQuery(query)
+              .then(querySucceeded, self.queryFailed);
+
+            function querySucceeded(data) {
+                console.log('getting some data');
+                return data.results[0];
+            }
+        }
+
+        function getTeamMemberById(id) {
+
+            var self = this;
+
+            var query = entityQuery.from("TeamMembers")
+              .where('id', '==', id);
+
+            return self.manager.executeQuery(query)
+              .then(querySucceeded, self.queryFailed);
+
+            function querySucceeded(data) {
+                return data.results[0];
+            }
+        }
+
+        function getNotPaidTeamMemberCountByTeamGuid(teamGuid) {
+            var self = this;
+            var query = entityQuery.from('GetNotPaidTeamMemberCountByTeamGuid')
+              .withParameters({
+                  id: teamGuid
+              });
+
+            return self.manager.executeQuery(query)
+              .then(querySucceeded, self._queryFailed);
+
+            function querySucceeded(data) {
+                return data.results[0];
+            }
+        }
+
+        function getTeamMembersByTeamId(id) {
+            var self = this;
+            var predicate = breeze.Predicate;
+            var p1 = new predicate("active", "==", true);
+            var p2 = new predicate("teamId", "==", Number(id));
+            var newPred = predicate.and(p1, p2);
+
+            var query = entityQuery.from("Teammembers")
+               .where(newPred);
+
+            return self.manager.executeQuery(query)
+              .then(querySucceeded, self.queryFailed);
+
+            function querySucceeded(data) {
+                return data.results;
+            }
+        }
+
+        function addTeamMember(obj) {
+            var self = this;
+            return self.manager.createEntity('TeamMember', obj);
+        }
+
+        function getTeamPaymentsByTeamId(id) {
+            var self = this;
+
+            var query = entityQuery.from("TeamMemberPayments")
+            // .where("teamId", "==", Number(id));
+
+            return self.manager.executeQuery(query)
+              .then(querySucceeded, self.queryFailed);
+
+            function querySucceeded(data) {
+                return data.results;
+            }
+        }
+
+        function getTeamMemberPaymentSumByTeamGuid(teamGuid) {
+            var self = this;
+            var query = entityQuery.from('GetTeamMemberPaymentSumByTeamGuid')
+              .withParameters({
+                  id: teamGuid
+              });
+
+            return self.manager.executeQuery(query)
+              .then(querySucceeded, self._queryFailed);
+
+            function querySucceeded(data) {
+                return data.results[0];
+            }
+        }
     }
-
-    // Allow this repo to have access to the Abstract Repo's functions,
-    // then put its own Ctor back on itself.
-    Ctor.prototype = new abstractRepository(Ctor);
-    Ctor.prototype.constructor = Ctor;
-    abstractRepository.extend(Ctor);
-
-    return Ctor;
-
-    function getAll() {
-      var self = this;
-      return entityQuery.from('ZZZ')
-        .using(self.manager).execute()
-        .then(querySucceeded, self._queryFailed);
-
-      function querySucceeded(data) {
-        return data.results;
-      }
-    }
-
-    function getTeamMemberPaymentInfoByTeamMemberGuid(guid) {
-      var self = this;
-      var query = entityQuery.from('getTeamMemberPaymentInfoByTeamMemberGuid')
-        .withParameters({
-          id: guid
-        });
-
-      return self.manager.executeQuery(query)
-        .then(querySucceeded, self._queryFailed);
-
-      function querySucceeded(data) {
-        return data.results[0];
-      }
-    };
-
-    function GetTeamInfoByRegistrationId(id) {
-      var self = this;
-      var query = entityQuery.from('GetTeamInfoByRegistrationId')
-        .withParameters({
-          id: id
-        });
-
-      return self.manager.executeQuery(query)
-        .then(querySucceeded, self._queryFailed);
-
-      function querySucceeded(data) {
-        return data.results[0];
-      }
-    };
-
-    function getTeamById(id) {
-
-      var self = this;
-
-      var query = entityQuery.from("Teams")
-        .where('id', '==', id);
-
-      return self.manager.executeQuery(query)
-        .then(querySucceeded, self.queryFailed);
-
-      function querySucceeded(data) {
-        return data.results[0];
-      }
-    }
-
-    function getTeamByGuid(guid) {
-
-        console.log('in the dccccc');
-
-      var self = this;
-
-      var query = entityQuery.from("Teams")
-        .where('teamGuid', '==', guid);
-
-      console.log('calling quesrty');
-      return self.manager.executeQuery(query)
-        .then(querySucceeded, self.queryFailed);
-
-      function querySucceeded(data) {
-          console.log('getting some data');
-        return data.results[0];
-      }
-    }
-
-    function getTeamMemberById(id) {
-
-      var self = this;
-
-      var query = entityQuery.from("TeamMembers")
-        .where('id', '==', id);
-
-      return self.manager.executeQuery(query)
-        .then(querySucceeded, self.queryFailed);
-
-      function querySucceeded(data) {
-        return data.results[0];
-      }
-    }
-
-    function getNotPaidTeamMemberCountByTeamGuid(teamGuid) {
-      var self = this;
-      var query = entityQuery.from('GetNotPaidTeamMemberCountByTeamGuid')
-        .withParameters({
-          id: teamGuid
-        });
-
-      return self.manager.executeQuery(query)
-        .then(querySucceeded, self._queryFailed);
-
-      function querySucceeded(data) {
-        return data.results[0];
-      }
-    }
-
-    function getTeamMembersByTeamId(id) {
-      var self = this;
-
-      var query = entityQuery.from("Teammembers")
-        .where("teamId", "==", Number(id));
-
-      return self.manager.executeQuery(query)
-        .then(querySucceeded, self.queryFailed);
-
-      function querySucceeded(data) {
-        return data.results;
-      }
-    }
-
-    function addTeamMember(obj) {
-      var self = this;
-      return self.manager.createEntity('TeamMember', obj);
-    }
-
-    function getTeamPaymentsByTeamId(id) {
-      var self = this;
-
-      var query = entityQuery.from("TeamMemberPayments")
-        // .where("teamId", "==", Number(id));
-
-      return self.manager.executeQuery(query)
-        .then(querySucceeded, self.queryFailed);
-
-      function querySucceeded(data) {
-        return data.results;
-      }
-    }
-
-    function getTeamMemberPaymentSumByTeamGuid(teamGuid) {
-      var self = this;
-      var query = entityQuery.from('GetTeamMemberPaymentSumByTeamGuid')
-        .withParameters({
-          id: teamGuid
-        });
-
-      return self.manager.executeQuery(query)
-        .then(querySucceeded, self._queryFailed);
-
-      function querySucceeded(data) {
-        return data.results[0];
-      }
-    }
-  }
 })();
--- app/registration/team/editteamController.js
+++ app/registration/team/editteamController.js
@@ -1,73 +1,80 @@
 ;
 (function () {
 
-  var controllerId = "EditTeamController";
+    var controllerId = "EditTeamController";
 
-  function Controller($scope, $location, $routeParams, $window, common, datacontext) {
+    function Controller($scope, $location, $routeParams, $window, $http, config, common, datacontext) {
 
-    var team = null;
+        var team = null;
 
-    function getTeam() {
-      return datacontext.team.getTeamById($routeParams.teamId)
-        .then(function (result) {
-          team = result;
-          $scope.team = team;
-          console.log("Team:", $scope.team);
+        function getTeam() {
+            return datacontext.team.getTeamById($routeParams.teamId)
+              .then(function (result) {
+                  team = result;
+                  $scope.team = team;
+                  console.log("Team:", $scope.team);
 
-        });
-    }
-
-    function getPlayers() {
-      return datacontext.team.getTeamMembersByTeamId($routeParams.teamId)
-        .then(function (members) {
-          console.log("members:", members);
-          // $scope.players = members;
-          $scope.players = [];
-          var player;
-          for (var i = 0; i < members.length; i++) {
-            player = members[i];
-            $scope.players.push(player);
-          }
-        });
-    }
-
-    var promises = [
-   getTeam(),
-   getPlayers()
-  ];
-    common.activateController(promises, controllerId)
-      .then(function () {})
-      .finally(function () {});
-
-    $scope.addPlayer = function (name, email) {
-      $scope.players.push({
-        name: null,
-        email: null,
-        position: null
-      });
-    };
-
-    $scope.editTeam = function () {
-      team.name = $scope.team.name;
-      for (var i = 0; i < $scope.players.length; i++) {
-        if (typeof $scope.players[i].id == "undefined") {
-          if ($scope.players[i].name && $scope.players[i].email) {
-            datacontext.team.addTeamMember({
-              teamId: $routeParams.teamId,
-              name: $scope.players[i].name,
-              email: $scope.players[i].email,
-              position: $scope.players[i].position,
-              active: true
-            });
-          }
+              });
         }
-      }
-      datacontext.save()
-        .then(function () {
-          $window.history.back();
-        });
-    };
-  }
 
-  angular.module("evReg").controller(controllerId, ["$scope", "$location", "$routeParams", "$window", "common", "datacontext", Controller]);
+        function getPlayers() {
+            return datacontext.team.getTeamMembersByTeamId($routeParams.teamId)
+              .then(function (members) {
+                  console.log("members:", members);
+                  // $scope.players = members;
+                  $scope.players = [];
+                  var player;
+                  for (var i = 0; i < members.length; i++) {
+                      player = members[i];
+                      $scope.players.push(player);
+                  }
+              });
+        }
+
+        var promises = [
+       getTeam(),
+       getPlayers()
+        ];
+        common.activateController(promises, controllerId)
+          .then(function () { })
+          .finally(function () { });
+
+        $scope.addPlayer = function (name, email) {
+            $scope.players.push({
+                name: null,
+                email: null,
+                position: null
+            });
+        };
+
+        $scope.editTeam = function () {
+            var item = [];
+            team.name = $scope.team.name;
+            for (var i = 0; i < $scope.players.length; i++) {
+                if (typeof $scope.players[i].id == "undefined") {
+                    if ($scope.players[i].name && $scope.players[i].email) {
+                        item.push(datacontext.team.addTeamMember({
+                            teamId: $routeParams.teamId,
+                            name: $scope.players[i].name,
+                            email: $scope.players[i].email,
+                            position: $scope.players[i].position,
+                            active: true
+                        }));
+                    }
+                }
+            }
+            console.log(datacontext);
+            datacontext.save()
+                  .then(function () {
+                     for (var i = 0; i < item.length; i++) {
+                         $http.post(config.apiPath + 'api/mail/SendTeamPlayerInviteMail/' + item[i].id);
+                      }
+                  })
+            .then(function () {
+                $window.history.back();
+            });
+        };
+    }
+
+    angular.module("evReg").controller(controllerId, ["$scope", "$location", "$routeParams", "$window", "$http","config", "common", "datacontext", Controller]);
 })();
