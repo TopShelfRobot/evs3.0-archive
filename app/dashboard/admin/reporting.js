18034640911def2cddb7a1d2208424ea95df11ec
Wil Goolsby <wgoolsby@me.com>
2015-04-07 17:10:04 -0400

    Add reporting grids

--- app/dashboard/admin/reporting.js
+++ app/dashboard/admin/reporting.js
@@ -1,37 +1,148 @@
-﻿(function () {
-    'use strict';
-    var controllerId = 'reporting';
-    angular.module('app').controller(controllerId, ['common', 'datacontext', reporting]);
+﻿(function() {
+	'use strict';
+	var controllerId = 'reporting';
+	angular.module('app').controller(controllerId, ['common', 'datacontext', 'config', reporting]);
 
-    function reporting(common, datacontext) {
-        var getLogFn = common.logger.getLogFn;
-        var log = getLogFn(controllerId);
+	function reporting(common, datacontext, config) {
+		var getLogFn = common.logger.getLogFn;
+		var log = getLogFn(controllerId);
 
-        var vm = this;
+		var vm = this;
+		vm.title = 'app';
+		vm.reports = [];
+		vm.ownerId = config.owner.ownerId;
 
-        vm.title = 'app';
+		activate();
 
-        vm.reports = [];
+		function activate() {
+			var promises = [paymentGrid(), rosterGrid()];
+			common.activateController(promises, controllerId)
+				.then(function() {
+					//log('Activated reporting View');
+				});
+		}
+
+		function paymentGrid() {
+			var teamPaymentReportApi = config.remoteApiName + 'widget/getTeamPaymentInfoByEventureId/2';
+			vm.paymentGridOptions = {
+				toolbar: ['excel'],
+				excel: {
+					fileName: 'TeamPayments.xlsx',
+					filterable: true
+				},
+				dataSource: {
+					type: 'json',
+					transport: {
+						read: teamPaymentReportApi
+					},
+					schema: {
+						model: {
+							fields: {
+								name: {
+									type: 'string'
+								}
+							}
+						}
+					},
+					pageSize: 10,
+					serverPaging: false,
+					serverSorting: false
+				},
+				sortable: true,
+				pageable: true,
+				filterable: {
+					mode: "row"
+				},
+				columns: [{
+					field: 'teamName',
+					title: 'Team Name',
+					width: 250
+				}, {
+					field: 'amountPaid',
+					title: 'Amount Paid',
+					width: 100,
+					format: '{0:c}'
+				}, {
+					field: 'captain',
+					title: 'Captain',
+					width: 250
+				}, {
+					field: 'email',
+					title: 'Email',
+					width: 250
+				}, {
+					field: 'balance',
+					title: 'Balance',
+					width: 100,
+					format: '{0:c}'
+				}, {
+					field: 'amountOwed',
+					title: 'Amount Owed',
+					width: 100,
+					format: '{0:c}'
+				}]
+			};
+		}
+
+		function rosterGrid() {
+			var teamRosterReportApi = config.remoteApiName + 'widget/GetTeamRosterInfoByEventureId/2';
+			vm.rosterGridOptions = {
+				toolbar: ['excel'],
+				excel: {
+					fileName: 'TeamRoster.xlsx',
+					filterable: true
+				},
+				dataSource: {
+					type: 'json',
+					transport: {
+						read: teamRosterReportApi
+					},
+					pageSize: 10,
+					serverPaging: false,
+					serverSorting: false
+				},
+				sortable: true,
+				pageable: true,
+				filterable: {
+					mode: "row"
+				},
+				columns: [{
+					field: 'memberName',
+					title: 'Member Name',
+					width: 250
+				}, {
+					field: 'email',
+					title: 'Email',
+					width: 250
+				}, {
+					field: 'phoneMobile',
+					title: 'Phone',
+					width: 150
+				}, {
+					field: 'teamName',
+					title: 'Team Name',
+					width: 250
+				}, {
+					field: 'postion',
+					title: 'Team Role',
+					width: 150
+				}, {
+					field: 'shirtSize',
+					title: 'Shirt Size',
+					width: 100
+				}, {
+					field: 'emergencyContact',
+					title: 'Emergency Contact',
+					width: 200
+				}, {
+					field: 'emergencyPhone',
+					title: 'Emergency Phone',
+					width: 150
+				}]
+			};
+		}
 
 
-        activate();
 
-        function activate() {
-            //var promises = [getMessageCount(), getPeople()];
-            common.activateController(getReports(), controllerId)
-                .then(function () {
-                    //log('Activated reporting View');
-                });
-        }
-
-      function getReports() {
-          return datacontext.analytic.getReportsByOwnerId(1)
-          //return datacontext.getPeople()
-              .then(function (data) {
-                  //alert(data)
-                  vm.reports = data;
-                  return vm.reports;
-              });
-      }
-    }
+	}
 })();
