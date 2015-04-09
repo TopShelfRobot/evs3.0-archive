530b105625c5f6e82f75a421f2695c821a101b97
Wil Goolsby <wgoolsby@me.com>
2015-04-09 16:04:36 -0400

    attempt to load straight to user-profile

--- app/registration/config.route.js
+++ app/registration/config.route.js
@@ -1,4 +1,4 @@
-(function () {
+(function() {
 	'use strict';
 
 	var app = angular.module('evReg');
@@ -10,12 +10,12 @@
 	app.config(['$routeProvider', 'reg.routes', routeConfigurator]);
 
 	function routeConfigurator($routeProvider, routes) {
-		routes.forEach(function (r) {
+		routes.forEach(function(r) {
 			// $routeProvider.when(r.url, r.config);
 			setRoute(r.url, r.config);
 		});
 		$routeProvider.otherwise({
-			redirectTo: '/eventure'
+			redirectTo: '/user-profile'
 		});
 
 
@@ -38,8 +38,7 @@
 
 	// Define the routes
 	function getRoutes() {
-		return [
-			{
+		return [{
 				url: '/eventure',
 				config: {
 					title: 'Eventure',
@@ -49,7 +48,7 @@
 						content: 'Events'
 					}
 				}
-   }, {
+			}, {
 				url: '/eventure/:eventureId/list/',
 				config: {
 					title: 'Eventure List',
@@ -59,7 +58,7 @@
 						content: 'Participants'
 					}
 				}
-   }, {
+			}, {
 				url: '/eventure/:eventureId/list/:listId/team',
 				config: {
 					title: 'Create Team',
@@ -69,7 +68,7 @@
 						content: 'Enterprise'
 					}
 				}
-   }, {
+			}, {
 				url: '/eventure/:eventureId/list/:listId/team/:teamId/payment',
 				config: {
 					title: 'Team Payment',
@@ -79,146 +78,152 @@
 						content: 'Coupons & Addons'
 					}
 				}
-   }, {
+			}, {
 				url: '/team/:teamGuid/member/:teamMemberGuid/payment',
 				config: {
 					title: 'Member Payment',
 					templateUrl: 'app/registration/team/memberPayment.part.html'
 				}
-   }, {
+			}, {
 				url: '/participant',
 				config: {
 					title: 'Participant',
 					templateUrl: 'app/registration/profile/setparticipant.part.html'
 				}
-   }, {
+			}, {
 				url: '/participant/add',
 				config: {
 					title: 'Add Participant',
 					templateUrl: 'app/registration/profile/addparticipant.part.html'
 				}
-   }, {
+			}, {
 				url: '/participant/:partId',
 				config: {
 					title: 'Edit Participant',
 					templateUrl: 'app/registration/profile/setparticipant.part.html'
 				}
-   }, {
+			}, {
 				url: '/eventure/:eventureId/list/:listId/questions',
 				config: {
 					title: 'Questions',
 					templateUrl: 'app/registration/standard/questions.part.html'
 				}
-   }, {
+			}, {
 				url: '/confirm',
 				config: {
 					title: 'Confirm',
 					templateUrl: 'app/registration/payment/confirm.part.html'
 				}
-   }, {
+			}, {
 				url: '/team-receipt/:registrationId',
 				config: {
 					title: 'Receipt',
 					templateUrl: 'app/registration/payment/teamReceipt.part.html'
 				}
-   }, {
+			}, {
 				url: '/member-receipt/:teamGuid',
 				config: {
 					title: 'Receipt',
 					templateUrl: 'app/registration/payment/teamMemberReceipt.part.html'
 				}
-   }, {
+			}, {
 				url: '/orderreceipt/:orderId',
 				config: {
 					title: 'Receipt Cart',
 					templateUrl: 'app/registration/payment/orderreceipt.part.html'
 				}
-   }, {
+			}, {
 				url: '/new-user/add',
 				config: {
 					title: 'Add User Profile',
 					templateUrl: 'app/registration/profile/addProfile.part.html'
 				}
-   }, {
+			}, {
 				url: '/user-profile/:participantGuid',
 				config: {
 					title: 'User Profile',
 					templateUrl: 'app/registration/profile/profile.part.html'
 				}
-   }, {
+			}, {
+				url: 'user-profile',
+				config: {
+					title: 'User Profile',
+					templateUrl: 'app/registration/profile/profile.part.html'
+				}
+			}, {
 				url: '/editteam/:teamId',
 				config: {
 					title: 'Edit Team',
 					templateUrl: 'app/registration/team/editteam.part.html'
 				}
-   }, {
+			}, {
 				url: '/registration/:regId',
 				config: {
 					title: 'Edit Registration',
 					templateUrl: 'app/registration/profile/registrationedit.part.html'
 				}
-   }, {
+			}, {
 				url: '/registration/:regId/edit',
 				config: {
 					title: 'Edit Registration',
 					templateUrl: 'app/registration/profile/registrationEditConfirm.part.html'
 				}
-   }, {
+			}, {
 				url: '/registration/:regId/transferQuestions',
 				config: {
 					title: 'Edit Registration',
 					templateUrl: 'app/registration/profile/transferQuestions.part.html'
 				}
-   }, {
+			}, {
 				url: '/shoppingcart',
 				config: {
 					title: 'Shopping Cart',
 					templateUrl: 'app/registration/_layout/shoppingcart.part.html'
 				}
-   }, {
+			}, {
 				url: '/login',
 				config: {
 					title: 'Login',
 					templateUrl: 'app/registration/authentication/login.part.html'
 				}
-   }, {
+			}, {
 				url: '/login/:requestPath*',
 				config: {
 					title: 'Login',
 					templateUrl: 'app/registration/authentication/login.part.html'
 				}
-   }, {
+			}, {
 				url: '/signup',
 				config: {
 					title: 'Sign-Up',
 					templateUrl: 'app/registration/authentication/signup.part.html'
 				}
-   }, {
+			}, {
 				url: '/forgotpassword',
 				config: {
 					title: 'Forgot Password',
 					templateUrl: 'app/registration/authentication/forgotpassword.part.html'
 				}
-   }, {
+			}, {
 				url: '/resetpassword',
 				config: {
 					title: 'Reset Password',
 					templateUrl: 'app/registration/authentication/resetpassword.part.html'
 				}
-   }, {
+			}, {
 				url: '/policies',
 				config: {
 					title: 'Policies',
 					templateUrl: 'app/registration/standard/policies.part.html'
 				}
-	 }
-   //, {
-   //    url: '/resetpassword/:token',
-   //    config: {
-   //        title: 'Reset Password',
-   //        templateUrl: 'app/registration/views/resetpassword.part.html'
-   //    }
-   //}
-  ];
+			}
+			//, {
+			//    url: '/resetpassword/:token',
+			//    config: {
+			//        title: 'Reset Password',
+			//        templateUrl: 'app/registration/views/resetpassword.part.html'
+			//    }
+			//}
+		];
 	}
 })();
