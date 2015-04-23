e776d17ec2cbb8922d5114bd5ad6161fb4c8b61c
Wil Goolsby <wgoolsby@me.com>
2015-04-23 16:42:41 -0400

    chart titles, treemap

--- app/dashboard/enterprise/dashboard.js
+++ app/dashboard/enterprise/dashboard.js
@@ -8,46 +8,41 @@
 		var log = getLogFn(controllerId);
 
 		var vm = this;
-		vm.title = 'app';
-		vm.ownerId = config.owner.ownerId;
-		vm.year = new Date().getFullYear();
+		var ownerId = config.owner.ownerId;
+		var year = new Date().getFullYear();
+		//Graph Apis
+		var genderByYearApi = config.remoteApiName + 'widget/GetGenderInfoByYear/' + year;
+		var ageByYearApi = config.remoteApiName + 'widget/GetAgeInfoByYear/' + year;
+		var zipByYearApi = config.remoteApiName + 'widget/GetZip/' + year;
+		var capacityByYearApi = config.remoteApiName + 'widget/GetCapacityRegDialsByYear/' + year;
+		var trendByOwnerApi = config.remoteApiName + 'widget/GetTrendByOwnerId/' + ownerId;
+		var revenueByOwnerApi = config.remoteApiName + 'widget/GetRevenueByOwnerId/' + ownerId;
+		//Set Default Chart Options
 		vm.chartOptions = {
 			theme: 'material',
 			legend: {
 				visible: false
 			},
 		};
-		vm.map = {
-			center: {
-				latitude: 45,
-				longitude: -73
-			},
-			zoom: 8
-		};
-		//Graph Apis
-		var overviewOwnerApi = config.remoteApiName + 'widget/GetOwnerGraph/' + vm.ownerId;
-		var genderByYearApi = config.remoteApiName + 'widget/GetGenderInfoByYear/' + vm.year;
-		var ageByYearApi = config.remoteApiName + 'widget/GetAgeInfoByYear/' + vm.year;
-		var zipByYearApi = config.remoteApiName + 'widget/GetZip/' + vm.year;
-		var capacityByYearApi = config.remoteApiName + 'widget/GetCapacityRegDialsByYear/' + vm.year;
-		var trendByOwnerApi = config.remoteApiName + 'widget/GetTrendByOwnerId/' + vm.ownerId;
-		var revenueByOwnerApi = config.remoteApiName + 'widget/GetRevenueByOwnerId/' + vm.ownerId;
-
+		// vm.map = {
+		// 	center: {
+		// 		latitude: 45,
+		// 		longitude: -73
+		// 	},
+		// 	zoom: 8
+		// };
 		activate();
 
 		function activate() {
-			var promises = [
-				CapacityByYearRadial()
-			];
+			var promises = [CapacityByYearRadial()];
 
 			common.activateController(promises, controllerId)
 				.then(function() {
-					uiGmapGoogleMapApi.then(function(map) {
-						google.maps.event.trigger(map, 'resize');
-					});
+					// uiGmapGoogleMapApi.then(function(map) {
+					// 	google.maps.event.trigger(map, 'resize');
+					// });
 				});
 		}
-
 
 		function CapacityByYearRadial() {
 			$http.get(capacityByYearApi).then(
@@ -95,18 +90,18 @@
 			dataSource: {
 				transport: {
 					read: {
-						url: "app/dashboard/enterprise/zipcode.json",
-						dataType: "json"
+						url: 'app/dashboard/enterprise/zipcode.json',
+						dataType: 'json'
 					}
 				},
 				schema: {
 					model: {
-						children: "items"
+						children: 'items'
 					}
 				}
 			},
-			valueField: "value",
-			textField: "name"
+			valueField: 'value',
+			textField: 'name'
 		};
 
 		function TrendsByOwner() {
@@ -130,57 +125,5 @@
 				}
 			});
 		}
-
-		function ownerOverviewChart() {}
-		vm.OverviewOwnerChart = {
-			theme: 'material',
-			dataSource: {
-				transport: {
-					read: {
-						url: overviewOwnerApi,
-						dataType: 'json'
-					}
-				}
-			},
-			title: {
-				text: 'Eventure Overview'
-			},
-			legend: {
-				position: 'bottom'
-			},
-			series: [{
-				type: 'column',
-				field: 'Rev',
-				name: 'Revenue',
-				axis: 'Revenue'
-			}, {
-				type: 'line',
-				field: 'Regs',
-				name: 'Registrations',
-				axis: 'Registrations'
-			}],
-			valueAxis: [{
-				name: 'Revenue',
-				title: {
-					text: 'Revenue'
-				}
-			}, {
-				name: 'Registrations',
-				title: {
-					text: 'Registrations'
-				}
-			}],
-			categoryAxis: {
-				field: 'Month',
-				majorGridLines: {
-					visible: false
-				}
-			},
-			tooltip: {
-				visible: true,
-				template: '#= series.name #: #= value #'
-			}
-		};
-
 	}
 })();
